ed8931848473ce3033bea3fa2a996c00e4de8a82
Sean Anderson <github@seananderson.ca>
2013-03-21 14:54:45 -0700

    initial commit

---
+++ R/get_rec_devs.R
@@ -0,0 +1,13 @@
+#' Extract recruitment deviations from the par file
+#' 
+#' @param working_folder The folder with SS3 output.
+
+get_rec_devs <- function(working_folder) {
+  SS_par <- scan(file=paste(working_folder, "/ss3.par", sep=""), 
+    what="character") 
+  rec.dev.find <- find_item(SS_par,
+    what_vec=c("recdev1","Fcast_recruitments"))
+  Rec_dev <- as.numeric(SS_par[rec.dev.find[1]:rec.dev.find[2]])
+  return(Rec_dev)
+}
+
