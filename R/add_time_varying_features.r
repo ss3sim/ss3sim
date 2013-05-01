d61264e80c22cccff17a72e1640b35fa9f2d7867
Sean Anderson <github@seananderson.ca>
2013-05-01 13:51:30 -0700

    Make variable names consistent with other functions

--- R/add_time_varying_features.r
+++ R/add_time_varying_features.r
@@ -1,42 +1,41 @@
 #' Methods to include time varying param features
 #'
-#' @param Nblocks Number of blocks
-#' @param Blockpattern Block pattern 
-#' @param Deviations
-#' @param ctl.file.in Input control file name
-#' @param ctl.file.out Output control file name
-#' @param Dat.file .dat file name
-#' @param par.file .par file name 
-#' @param how.time.varying How time varying parameters are added.
-#' Options are "env" for environment, "block" for block, or "dev" for
-#' deviations. 
-#'
+#' @param n_blocks Number of blocks
+#' @param block_pattern Block pattern 
+#' @param dev Deviations
+#' @param ctl_file_in Input control file name
+#' @param ctl_file_out Output control file name
+#' @param dat_file .dat file name
+#' @param par_file .par file name 
+#' @param how_time_varying How time varying parameters are added.
+#' Options are \code{"env"} for environment, \code{"block"} for block,
+#' or \code{"dev"} for deviations. 
 #' @export
 			
 add_time_varying_features <- function(
-  Nblocks, 
-  Blockpattern, 
-  Dev,
-  ctl.file.in ="control.ss_new", 
-  ctl.file.out ="YTF.ctl",
-  Dat.file="YTF.dat", 
-  par.file="ss3.par", 
-  how.time.varying=c("env", "block", "dev")) {
+  n_blocks, 
+  block_pattern, 
+  dev,
+  ctl_file_in ="control.ss_new", 
+  ctl_file_out ="YTF.ctl",
+  dat_file="YTF.dat", 
+  par_file="ss3.par", 
+  how_time_varying=c("env", "block", "dev")) {
 
   require(r4ss)
 
-  how.time.varying <- how.time.varying[1]
-  if(!how.time.varying %in% c("env", "block", "dev")) 
-    stop("how.time.varying must be one of env, block, dev")
+  how_time_varying <- how_time_varying[1]
+  if(!how_time_varying %in% c("env", "block", "dev")) 
+    stop("how_time_varying must be one of env, block, dev")
 
   ## To read in the control file and create an R object to modify afterwards
-  SS_ctl <- readLines(ctl.file.in)
+  SS_ctl <- readLines(ctl_file_in)
   ## To read in the dat file 
-  SS_dat <- SS_readdat(Dat.file, verbose = FALSE, echoall = FALSE, section = NULL)
+  SS_dat <- SS_readdat(dat_file, verbose = FALSE, echoall = FALSE, section = NULL)
   year.beg=SS_dat$styr
   year.end=SS_dat$endyr
 
-  if(how.time.varying == "block")
+  if(how_time_varying == "block")
   {
     # change the block pattern
     ch1 <- grep("#_Nblock_Patterns", SS_ctl)
@@ -44,8 +43,8 @@ add_time_varying_features <- function(
     val1 <- as.numeric(as.vector(substr(SS_ctl[ch1], start=1, stop=ch2-1)))
     if (val1>1) print("Beware! There are more than ONE block pattern in this ctl file")
     SS_ctl[ch1] = paste(1, "#_Nblock_Patterns", sep=" ")
-    SS_ctl[ch1+1] = paste(Nblocks, "#_blocks_per_pattern ", sep=" ")
-    SS_ctl[ch1+3] = paste(Blockpattern, collapse=" ")
+    SS_ctl[ch1+1] = paste(n_blocks, "#_blocks_per_pattern ", sep=" ")
+    SS_ctl[ch1+3] = paste(block_pattern, collapse=" ")
     # now add the time varying feature into the model	 
     # find the line specifying natural mortality params
     ch3 <- grep("# NatM_p_1_Fem_GP_1", SS_ctl)
@@ -71,7 +70,7 @@ add_time_varying_features <- function(
     SS_ctl[ch5+1] <- paste(c(val2, " # env link specification i.e fixed to 1"), collapse=" ")
   }
 
-  if(how.time.varying == "env")
+  if(how_time_varying == "env")
   {
     # add the time varying feature into the ctl file	 
     # find the line specifying natural mortality params
@@ -97,11 +96,11 @@ add_time_varying_features <- function(
     val2 <- c(-1, 2, 1, 0, -1, 99, -2)
     SS_ctl[ch5+1] <- paste(c(val2, " # env link specification i.e fixed to 1"), collapse=" ")
 
-    writeLines(SS_ctl, con= ctl.file.out)
+    writeLines(SS_ctl, con= ctl_file_out)
 
     # add the time varying feature into the dat file	 
     # Now put the environmental covariate into the dat file
-    SS_data <- readLines(con = Dat.file)
+    SS_data <- readLines(con = dat_file)
     ch1d <- grep("#_N_environ_variables", SS_data) 
     ch2d <- regexpr(" #_N_environ_variables", SS_data[ch1d], fixed=TRUE)[1]
     SS_data[ch1d] <- paste(1, " #_N_environ_variables")
@@ -113,15 +112,15 @@ add_time_varying_features <- function(
     Last_piece = SS_data[(ch3d+1):endfile]				
     # to see whether such environemental data exist already or not
     ch5d <- grep("#_N_sizefreq_methods", SS_data, fixed=TRUE) 
-    if((ch5d-ch3d)==1) env.dat <- data.frame(Year=year.beg:year.end, Variable=1, Value=Dev)
+    if((ch5d-ch3d)==1) env.dat <- data.frame(Year=year.beg:year.end, Variable=1, Value=dev)
     if((ch5d-ch3d)>1) { env.dat <- c() ; print("WARNINGS: env data already exist. Check") }
     # combine back everything
     New.dat=c(First_piece, apply(env.dat, 1, function(x) paste(x, collapse=" ")), Last_piece)
     # write output	
-    writeLines(New.dat, con= Dat.file)
+    writeLines(New.dat, con= dat_file)
   }
 
-  if(how.time.varying == "dev")
+  if(how_time_varying == "dev")
   {
     # add the time varying feature into the ctl file	 
     # find the line specifying natural mortality params
@@ -143,19 +142,19 @@ add_time_varying_features <- function(
     val[12] = 1				
     SS_ctl[ch3] <- paste(c(val, "# NatM_p_1_Fem_GP_1"), collapse=" ")
     # Now specify the custom MG-env setup		
-    writeLines(SS_ctl, con= ctl.file.out)
+    writeLines(SS_ctl, con= ctl_file_out)
 
     # add grwoth dev estimates into the par file	 
-    SS_par <- readLines(con = par.file)
+    SS_par <- readLines(con = par_file)
     ch1 <- grep("# SR_parm[1]:", SS_par, fixed=TRUE)
     MGdev.exist = sum(as.numeric((grep("# MGparm_dev:", SS_par, fixed=TRUE))))
     length.file=length(SS_par)
     beg.file=SS_par[1:(ch1-1)]
     end.file=SS_par[(ch1):length.file]
-    if(MGdev.exist==0) {to.add <- c("# MGparm_dev:", paste(Dev, collapse=" "))} 
+    if(MGdev.exist==0) {to.add <- c("# MGparm_dev:", paste(dev, collapse=" "))} 
     if(MGdev.exist!=0) {print("WARNINGS: MGparm_dev line already exist"); to.add <- NULL}
     new.par=c(beg.file, to.add, end.file)
-    writeLines(new.par, con = par.file)
+    writeLines(new.par, con = par_file)
   }
 }
 
@@ -178,7 +177,7 @@ add_time_varying_features <- function(
 		# year.beg=SS_dat$styr
 		# year.end=SS_dat$endyr
 	# # specifying the different blockpattern (only used for the operating model)
-		# Blockpattern = c(year.beg, year.beg+50, year.beg+51, year.end)
+		# block_pattern = c(year.beg, year.beg+50, year.beg+51, year.end)
 	# # Block value
 		# Env = c(rep(0, 50), rep(0.5, 50))
 
@@ -190,8 +189,8 @@ add_time_varying_features <- function(
 # SS_data <- readLines(con = paste(working_folder, "/", "YTF.dat", sep=""))
 # year.beg=SS_dat$styr
 # year.end=SS_dat$endyr
-#Dev = c(rep(0.2, (2000-1913+1)), rep(0.1, (2012-2000)))-0.2
-#Include_timevarying_param(working_folder=working_folder, ctl.file.in ="control.ss_new", ctl.file.out ="YTF.ctl", Dat.file="YTF.dat", par.file="ss3.par", Nblocks, Blockpattern, Dev=Dev, how.time.varying="dev")
+#dev = c(rep(0.2, (2000-1913+1)), rep(0.1, (2012-2000)))-0.2
+#Include_timevarying_param(working_folder=working_folder, ctl_file_in ="control.ss_new", ctl_file_out ="YTF.ctl", dat_file="YTF.dat", par_file="ss3.par", n_blocks, block_pattern, dev=dev, how_time_varying="dev")
 
 # direc = paste(dir_folder, "/Yellowtail - step1 - Copy", sep="")
 # direc = paste(dir_folder, "trial", sep="")
