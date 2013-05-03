5642a225d367f2cce943af0b3062717cdefc9ef7
Sean Anderson <github@seananderson.ca>
2013-05-02 17:55:50 -0700

    Remove cast_recruitments code from get_rec_devs
    Whether this was present depends on the forecasting settings. By just
    grepping for 'F' we avoid this problem. Kelli discovered this bug and
    suggested the fix.

--- R/get_rec_devs.r
+++ R/get_rec_devs.r
@@ -8,7 +8,7 @@ get_rec_devs <- function(working_folder) {
   SS_par <- scan(file=paste(working_folder, "/ss3.par", sep=""), 
     what="character") 
   rec.dev.find <- find_item(SS_par,
-    what_vec=c("recdev1","Fcast_recruitments"))
+    what_vec=c("recdev1","F"))
   Rec_dev <- as.numeric(SS_par[rec.dev.find[1]:rec.dev.find[2]])
   return(Rec_dev)
 }
