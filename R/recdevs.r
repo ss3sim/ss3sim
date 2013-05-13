ecf9b98f5e38d56eb890a3e0d54dc83b06778c8a
Sean Anderson <github@seananderson.ca>
2013-05-13 12:15:44 -0700

    Add rec dev data

---
+++ R/recdevs.r
@@ -0,0 +1,12 @@
+#' Randomly generated recruitment deviations
+#'
+#' These data are randomly generated recruitment deviations with mean
+#' 0 and standard deviation 1. They were generated with
+#' \code{set.seed(999);recdevs <- matrix(nrow = 100, ncol = 100, data
+#' = rnorm(100*100)}
+#'
+#' @name recdevs
+#' @docType data
+#' @keywords data
+NULL
+
