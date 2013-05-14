25944ebede1fbc192ca303924bf5cd9e0725a61a
Sean Anderson <github@seananderson.ca>
2013-05-13 19:37:53 -0700

    Switch to run_model from run_om

--- R/run_model.r
+++ R/run_model.r
@@ -1,10 +1,11 @@
 #' Run the operating models for a specified set of scenario IDs
 #' @param scenarios Which scenarios to run.
 #' @param iterations Which iterations to run.
+#' @param type Are you running the operating or estimation models?
 #' @export
 #' @author Sean Anderson
 
-run_om <- function(scenarios, iterations) {
+run_model <- function(scenarios, iterations, type = c("om", "em")) {
 
 ss_call <- switch(.Platform$OS.type, 
   unix = "SS3",
@@ -24,8 +25,8 @@ sep <- switch(.Platform$OS.type,
   for(sc in scenarios) {
     for(it in iterations) {
       om <- pastef(sc, it)
-      print(paste0("Running scenario: ", sc, "; iteration: ", it)) 
-      system2(paste0(dir_switch, pastef(sc, it), sep, ss_call)) 
+      print(paste0("Running ", type, " for scenario: ", sc, "; iteration: ", it)) 
+      system2(paste0(dir_switch, pastef(sc, it, type), sep, ss_call)) 
     }
   }
 }
