7ce3d9a1cc63a6e6f120bc3ebd0a14b4bf840146
Sean Anderson <github@seananderson.ca>
2013-05-14 22:50:15 -0700

    Add nohess option for OM call, adjust windows command

--- R/run_model.r
+++ R/run_model.r
@@ -9,17 +9,21 @@ run_model <- function(scenarios, iterations, type = c("om", "em")) {
 
   os <- .Platform$OS.type 
 
+  ss_options <- switch(type, om = "-nohess", em = "")
+
   for(sc in scenarios) {
     for(it in iterations) {
-      om <- pastef(sc, it)
-      print(paste0("Running ", type, " for scenario: ", sc, "; iteration: ", it)) 
+      #print(paste0("Running ", type, " for scenario: ", sc, ";
+      #iteration: ", it)) 
       if(os == "unix") {
-        system(paste("cd", pastef(sc, it, type), ";SS3")) 
+        system(paste("cd", pastef(sc, it, type), ";SS3", ss_options)),
       } else {
         warning("Windows command not tested yet")
-        shell(paste("cd", pastef(sc, it, type), "&SS3.exe")) 
+        shell(paste("cd", pastef(sc, it, type), "&ss3", ss_options),
+          invisible = TRUE) 
       }
     }
   }
+
 }
 
