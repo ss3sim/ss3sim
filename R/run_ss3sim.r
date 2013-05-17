eee3961181822a029015cb65dbc728a202d30a2e
Sean Anderson <github@seananderson.ca>
2013-05-17 14:38:45 -0700

    Restructure run_ss3sim; see details
    No longer allowing just om or em runs. Added Change M and change F
    sections. Fixed arguments to other functions.

--- R/run_ss3sim.r
+++ R/run_ss3sim.r
@@ -5,16 +5,14 @@
 #' output (add recruitment deviations, survey the data, etc.), and run
 #' the estimation model.
 #'
+#' @param iterations Which iterations to run. A numeric vector.
 #' @param scenarios Which scenarios to run.
-#' @param iterations Which iterations to run.
 #' @param index_params A named list containing all the
 #' \code{\link{change_index}} options.
 #' @param lcomp_params A named list containing all the
 #' \code{\link{change_lcomp}} options.
 #' @param agecomp_params A named list containing all the
 #' \code{\link{change_agecomp}} options.
-#' @param type Are you running the both the operating and estimation models or just one
-#' or the other?
 #' @export
 #' @details
 #' This function is written to be flexible. You can specify the index,
@@ -28,87 +26,100 @@
 # todo change params to args
 # todo change iter to reps
 
-run_ss3sim <- function(scenarios, iterations, index_params,
-  lcomp_params, agecomp_params, type = c("om and em", "om", "em")) {
+run_ss3sim <- function(iterations, scenarios, m_params, f_params,
+  index_params, lcomp_params, agecomp_params, om_model_dir,
+  em_model_dir) {
 
   # create default list objects
   #if(index_params == "default") data(default_index_params)
   #if(lcomp_params == "default") data(default_lcomp_params)
   #if(agecomp_params == "default") data(default_agecomp_params)
 
-  type <- type[1]
+  for(sc in scenarios) {
+    for(i in iterations) {
 
-  switch(type,
-    "om and em" = {
-      for(sc in scenarios) {
-        for(i in iterations) {
+      # Load recruitment deviation data
+      # data(recdevs) # now it is lazy loaded on package install
 
-          # Load recruitment deviation data
-          # data(recdevs) # now it is lazy loaded on package install
+      # Pull in sigma R from the operating model
+      sigmar <- get_sigmar(pastef(sc, i, "om", "om"))
 
-          # Pull in sigma R from the operating model
-          sigmar <- get_sigmar(pastef(sc, i, "om", "om"))
+      # Exponentiate with bias adjustment
+      sc_i_recdevs <- exp(sigmar * recdevs[, i] - (sigmar^2)/2)
 
-          # Exponentiate with bias adjustment
-          sc_i_recdevs <- exp(sigmar * recdevs[, i] - (sigmar^2)/2)
+      # Add new rec devs overwriting om/ss3.par
+      change_rec_devs(recdevs_new = sc_i_recdevs, file_in =
+        pastef(sc, i, "om", "ss3.par"), file_out = paste_f(sc, i,
+          "om", "ss3.par"))
 
-          # Add new rec devs overwriting om/ss3.par
-          change_rec_devs(recdevs_new = sc_i_recdevs, file_in =
-            pastef(sc, i, "om", "ss3.par"), file_out = paste_f(sc, i,
-              "om", "ss3.par"))
+      # Change M
+      with(m_params,
+        change_m(n_blocks          = n_blocks,
+                 block_pattern     = block_pattern,
+                 dev               = dev,
+                 ctl_file_in       = pastef(sc, i, "om", "om.ctl"),
+                 ctl_file_out      = pastef(sc, i, "om", "om.ctl"),
+                 dat_file          = pastef(sc, i, "om", "data.dat"),
+                 par_file          = pastef(sc, i, "om", "ss3.par"),
+                 how_time_varying  = how_time_varying)) 
 
-          # Run the operating model
-          run_ss3model(scenarios = sc, iterations = i, type = "om")
+      # Change F
+      with(f_params,
+        change_f(years       = years,
+                 years_alter = years_alter,
+                 fvals       = fvals,
+                 file_in     = pastef(sc, i, "om", "ss3.par"),
+                 file_out    = pastef(sc, i, "om", "ss3.par")))
 
-          # Survey biomass index
-          with(index_params, 
-            change_index(dat_file_in    = pastef(sc, i, "om", "data.dat"), 
-                         dat_file_out   = pastef(sc, i, "em", "data.dat"),
-                         start_surv     = start_surv,
-                         end_surv       = end_surv))
+      # Run the operating model
+      run_ss3model(scenarios = sc, iterations = i, type = "om")
 
-          # Add error in the length comp data
-          SS.dat = r4ss::SS_readdat(pastef(sc, i, "em", "data.dat"))
-          with(lcomp_params,
-          change_lcomp(infile       = SS.dat,
-                       outfile      = pastef(sc, i, "em", "data.dat"),
-                       distribution = multinomial,
-                       Nsamp        = Nsamp,
-                       minyear      = minyear,
-                       maxyear      = maxyear,
-                       years        = years,
-                       svyears      = svyears,
-                       lbin_method  = lbin_method,
-                       binwidth     = binwidth,
-                       minimum_size = minimum_size,
-                       maximum_size = maximum_size,
-                       N_lbins      = N_lbins,
-                       lbin_vector  = lbin_vector,
-                       lencomp      = lencomp))
+      # Survey biomass index
+      with(index_params, 
+        change_index(dat_file_in    = pastef(sc, i, "om", "data.dat"), 
+                     dat_file_out   = pastef(sc, i, "em", "data.dat"),
+                     start_surv     = start_surv,
+                     end_surv       = end_surv))
 
-          # Add error in the age comp data
-          SS.dat2 = r4ss::SS_readdat(pastef(sc, i, "em", "data.dat"))
-          with(agecomp_params, 
-            change_agecomp(infile        = SS.dat2,
-                           outfile       = pastef(sc, i, "em", "data.dat"),
-                           distribution  = distribution,
-                           Nsamp         = Nsamp,
-                           minyear       = minyear,
-                           maxyear       = maxyear,
-                           years         = years,
-                           svyears       = svyears,
-                           N_agebins     = N_agebins,
-                           agebin_vector = agebin_vector,
-                           agecomp       = agecomp))
-          warning("Manipulation steps aren't all implemented or checked yet!")
+      # Add error in the length comp data
+      SS.dat = r4ss::SS_readdat(pastef(sc, i, "em", "data.dat"))
+      with(lcomp_params,
+        change_lcomp(infile       = SS.dat,
+                     outfile      = pastef(sc, i, "em", "data.dat"),
+                     distribution = multinomial,
+                     Nsamp        = Nsamp,
+                     minyear      = minyear,
+                     maxyear      = maxyear,
+                     years        = years,
+                     svyears      = svyears,
+                     lbin_method  = lbin_method,
+                     binwidth     = binwidth,
+                     minimum_size = minimum_size,
+                     maximum_size = maximum_size,
+                     N_lbins      = N_lbins,
+                     lbin_vector  = lbin_vector,
+                     lencomp      = lencomp))
 
-          run_ss3model(scenarios = sc, iterations = i, type = "em")
-        } # end iterations
-      } # end scenarios
-    }, # end "om and em" switch option
-    "om" = run_ss3model(scenarios, iterations, type = "om"),
-    "em" = run_ss3model(scenarios, iterations, type = "em")
-    ) # end switch() call
+      # Add error in the age comp data
+      SS.dat2 = r4ss::SS_readdat(pastef(sc, i, "em", "data.dat"))
+      with(agecomp_params, 
+        change_agecomp(infile        = SS.dat2,
+                       outfile       = pastef(sc, i, "em", "data.dat"),
+                       distribution  = distribution,
+                       Nsamp         = Nsamp,
+                       minyear       = minyear,
+                       maxyear       = maxyear,
+                       years         = years,
+                       svyears       = svyears,
+                       N_agebins     = N_agebins,
+                       agebin_vector = agebin_vector,
+                       agecomp       = agecomp))
+
+      warning("Manipulation steps aren't all implemented or checked yet!")
+
+      run_ss3model(scenarios = sc, iterations = i, type = "em")
+    } # end iterations
+  } # end scenarios
 }
 
 
