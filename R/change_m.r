547f29cd8fbeae4860400e25e3d0043f338189f1
Sean Anderson <github@seananderson.ca>
2013-05-18 00:27:05 -0700

    Re-write of change_m by Carey

--- R/change_m.r
+++ R/change_m.r
@@ -1,91 +1,128 @@
 #' Methods to include time varying natural mortality features
 #'
-#' @param n_blocks Number of blocks
-#' @param block_pattern Block pattern. A vector of years when changes
-#' occur.
-#' @param dev A vector of deviations
-#' @param ctl_file_in Input control file name
-#' @param ctl_file_out Output control file name
-#' @param dat_file .dat file name
-#' @param par_file .par file name 
-#' @param how_time_varying How time varying parameters are added.
-#' Options are \code{"env"} for environment, \code{"block"} for block,
-#' or \code{"dev"} for deviations. 
-#' @export
-			
-change_m <- function(
-  n_blocks, 
-  block_pattern, 
-  dev,
-  ctl_file_in ="om.ctl", 
-  ctl_file_out ="om.ctl",
-  dat_file="data.dat", 
-  par_file="ss3.par", 
-  how_time_varying=c("env", "block", "dev")) {
+#' This function takes generic OM files for sardine, flatfish, and cod and
+#' inserts time-varying M. 
+#'
+#' @param how_time_varying The method for including time-varying M in
+#' the SS input files the 3 options are: \code{"env"} \code{"block"}
+#' or \code{"dev"}. 
+#' \itemize{
+#' \item "env" is to include an ADDITIVE functional linkage between
+#' environmental data and M where a link parameter is estimated: \code{M'(y) =
+#' M + link * env(y)}
+#' \item \code{"block"} is to allow M to be estimated separately in different
+#' pre-specified time blocks
+#' \item \code{"dev"} is to estimate a mean M and to estimate deviations about
+#' that mean
+#' }
+#' @param ctl_file_in Input control file name (before M estimation method is
+#' chosen)
+#' @param ctl_file_out Output control file name (resulting control file with M
+#' changed)
+#' @param dat_file \code{.dat} File name
+#' @param par_file \code{.par} File name 
+#' @param n_blocks: The number of time blocks within which M is estimated
+#' separately; Equal to 1 unless \code{how_time_varying} is \code{"block"}, but
+#' #' is not used in the function unless \code{how_time_varying} is
+#' \code{"block"}
+#' @param block_pattern Block pattern. A vector of years marking time blocks
+#' with separate Ms in each block - doesn't get used in the function if
+#' \code{how_time_varying} does not equal \code{"block"}
+#' @param dev: A vector of environmental data of length = length of *.dat
+#' endyr-startyr+1
+#' @author Kotaro Ono and Carey McGilliard
+#' @export 
+#'
+#' @details Although it appears that 3 options exist for how time-varying M is
+#' modeled within SS and within this function, right now only the
+#' how_time_varying = "env" (making m vary over time by including an ADDITIVE
+#' environmental linkage to M) has been tested. The \code{ctl_file_in}
+#' parameter needs to be a \code{.ss_new} file because the documentation in
+#' \code{.ss_new} files are automated and standardized; this function takes
+#' advantage of standard documentation used to figure out where additional
+#' lines need to be added to \code{.ctl} files to implement time-varying M. 
+#' 
+#' CAREFUL: currently the function will modify the \code{.dat} file
+#' (\code{dat_file}) that is input; make a copy of the \code{.dat}
+#' file under another name if you want to keep an unmodified copy 
+#'  
+#' NOTE: the user has to define an environmental data series such that
+#' the additive linkage creates the desired time-varying pattern in
+#' M. 
+#'
+#' @examples 
+#' \dontrun{
+#' # setwd()... simple SS3 example
+#' change_m(how_time_varying="env",ctl_file_in = "control.ss_new",dat_file =
+#' "simple.dat",par_file = "SS3.par",n_blocks = 1,block_pattern =
+#' c(1990,2001),dev = rnorm(31,mean = 0,sd = 0.6))
+#'
+#' # Other examples
+#' # setwd() sardine model
+#' change_m(how_time_varying="env",ctl_file_in = "control.ss_new",dat_file =
+#' "SardOM.dat",par_file = "SS3.par",n_blocks = 1,block_pattern =
+#' c(1990,2001),dev = rnorm(100,mean = 0,sd = 0.6))
+#'
+#' setwd() flatfish model
+#' change_m(how_time_varying="env",ctl_file_in = "control.ss_new",dat_file =
+#' "flatfish.dat",par_file = "SS3.par",n_blocks = 2,block_pattern =
+#' c(1990,2001),dev = rnorm(100,mean = 0,sd = 0.6))
+#' }
 
-  how_time_varying <- how_time_varying[1]
+change_m <- function(how_time_varying="env", ctl_file_in =
+  "control.ss_new", ctl_file_out = "Simple.ctl", dat_file =
+  "Simple.dat", par_file = "ss3.par", n_blocks = 1, block_pattern,
+  dev) {
+  
+  # how_time_varying <- how_time_varying[1] #In the future if people want to
+  # include more than one kind of time-varying M 
+  # we can make how_time_varying into a vector, but now it's not.
+  # Test to see if the argument how_time_varying was input correctly; stop
+  # running if it's wrong.
   if(!how_time_varying %in% c("env", "block", "dev")) 
-    stop("how_time_varying must be one of env, block, dev")
-
-  ## To read in the control file and create an R object to modify afterwards
+    stop("how_time_varying must be equal to env, block, or dev")
+  
+  # Read in the control and dat files and create R objects to modify
+  ## afterwards, read in .dat file
   SS_ctl <- readLines(ctl_file_in)
-  ## To read in the dat file 
   SS_dat <- r4ss::SS_readdat(dat_file, verbose = FALSE, echoall = FALSE, section = NULL)
+  
   year.beg=SS_dat$styr
   year.end=SS_dat$endyr
-
-  if(how_time_varying == "block")
-  {
-    # change the block pattern
-    ch1 <- grep("#_Nblock_Patterns", SS_ctl)
-    ch2 <- regexpr("#_Nblock_Patterns", SS_ctl[ch1])[1]
-    val1 <- as.numeric(as.vector(substr(SS_ctl[ch1], start=1, stop=ch2-1)))
-    if (val1>1) print("Beware! There are more than ONE block pattern in this ctl file")
-    SS_ctl[ch1] = paste(1, "#_Nblock_Patterns", sep=" ")
-    SS_ctl[ch1+1] = paste(n_blocks, "#_blocks_per_pattern ", sep=" ")
-    SS_ctl[ch1+3] = paste(block_pattern, collapse=" ")
-    # now add the time varying feature into the model	 
-    # find the line specifying natural mortality params
-    ch3 <- grep("# NatM_p_1_Fem_GP_1", SS_ctl)
-    ch4 <- regexpr("# NatM_p_1_Fem_GP_1", SS_ctl[ch3])[1]
-    val = strsplit(substr(SS_ctl[ch3], start=1, stop=ch4-1), " ")[[1]]	# extract the element of the character vector
-    val = as.numeric(val)	# make them a numeric vector
-    check = (is.na(val))
-    if (sum(check)>0) 
-    {
-      print("Some values are removed. Normally it is the white space but checking it...")
-      val = val[check==FALSE]	# only keep the ones that are non NAs
-      ifelse(length(val)==14, print("Everything is fine"), print("Something is wrong. Please check the control.ss.new file"))					
-    }	
-
-    # find the line specifying natural mortality params and specify the environmental link
-    val[8] <- -1
-    SS_ctl[ch3] <- paste(c(val, "# NatM_p_1_Fem_GP_1"), collapse=" ")
-    # Now specify the custom MG-env setup
-    ch5 <- grep("#_Cond 0  #custom_MG-env_setup (0/1)", SS_ctl, fixed=TRUE)
-    ch6 <- regexpr(" #custom_MG-env_setup (0/1)", SS_ctl[ch5], fixed=TRUE)[1]
-    SS_ctl[ch5] <- paste(0, "#custom_MG-env_setup (0/1)")
-    val2 <- c(-1, 2, 1, 0, -1, 99, -2)
-    SS_ctl[ch5+1] <- paste(c(val2, " # env link specification i.e fixed to 1"), collapse=" ")
-  }
-
-  if(how_time_varying == "env")
-  {
+  
+  # how_time_varying = "env" may be the only how_time_varying option that's
+  # working!!! Use it!!!
+  # "env" is the preferred method of doing time-varying M that we decided on as
+  # a class
+  if(how_time_varying == "env") {
     # add the time varying feature into the ctl file	 
     # find the line specifying natural mortality params
-    ch3 <- grep("# NatM_p_1_Fem_GP_1", SS_ctl)
-    ch4 <- regexpr("# NatM_p_1_Fem_GP_1", SS_ctl[ch3])[1]
-    val = strsplit(substr(SS_ctl[ch3], start=1, stop=ch4-1), " ")[[1]]	# extract the element of the character vector
-    val = as.numeric(val)	# make them a numeric vector
-    check = (is.na(val))
-    if (sum(check)>0) 
+
+    # Find the line # of the line with the character string "#
+    # NatM_p_1_Fem_GP_1" in SS_ctl
+    ch3a <- grep("# NatM_p_1_Fem_GP_1", SS_ctl) 
+    ch3<-ch3a[1]
+
+    # Extract every value that comes before "# NatM_p_1_Fem_GP_1" on
+    # line ch3 as characters in the character vector "val"
+    ch4 <- regexpr("# NatM_p_1_Fem_GP_1", SS_ctl[ch3])[1] 
+
+    # Extract every value that comes before "# NatM_p_1_Fem_GP_1" on
+    # line ch3 as characters in the character vector "val"
+    val = strsplit(substr(SS_ctl[ch3], start=1, stop=ch4-1), " ")[[1]]	
+    val = as.numeric(val)	# change val to a numeric vector
+    check = (is.na(val)) #check for missing values
+    if (sum(check)>0) #Extra QAQC check
     {
-      print("Some values are removed. Normally it is the white space but checking it...")
+      print("Some values are removed. Normally it is the white space but
+        checking it...")
       val = val[check==FALSE]	# only keep the ones that are non NAs
       ifelse(length(val)==14, print("Everything is fine"),
-        print("Something is wrong. Please check the control.ss.new file"))					
+             print("Something is wrong. Please check the control.ss.new
+               file"))					
     }	
-    # find the line specifying natural mortality params and specify the environmental link
+    # find the line specifying natural mortality params and specify
+    # the environmental link
     val[8] <- -1
     SS_ctl[ch3] <- paste(c(val, "# NatM_p_1_Fem_GP_1"), collapse=" ")
     # Now specify the custom MG-env setup
@@ -94,9 +131,9 @@ change_m <- function(
     SS_ctl[ch5] <- paste(0, "#custom_MG-env_setup (0/1)")
     val2 <- c(-1, 2, 1, 0, -1, 99, -2)
     SS_ctl[ch5+1] <- paste(c(val2, " # env link specification i.e fixed to 1"), collapse=" ")
-
+    
     writeLines(SS_ctl, con= ctl_file_out)
-
+    
     # add the time varying feature into the dat file	 
     # Now put the environmental covariate into the dat file
     SS_data <- readLines(con = dat_file)
@@ -110,7 +147,7 @@ change_m <- function(
     First_piece = SS_data[1:ch3d]
     Last_piece = SS_data[(ch3d+1):endfile]				
     # to see whether such environemental data exist already or not
-    ch5d <- grep("#_N_sizefreq_methods", SS_data, fixed=TRUE) 
+    ch5d <- grep("# N sizefreq methods to read", SS_data, fixed=TRUE) 
     if((ch5d-ch3d)==1) env.dat <- data.frame(Year=year.beg:year.end, Variable=1, Value=dev)
     if((ch5d-ch3d)>1) { env.dat <- c() ; print("WARNINGS: env data already exist. Check") }
     # combine back everything
@@ -118,9 +155,10 @@ change_m <- function(
     # write output	
     writeLines(New.dat, con= dat_file)
   }
-
-  if(how_time_varying == "dev")
-  {
+  
+  # We are not currently using how_time_varying = "dev" and this section may
+  # need updating!!!
+  if(how_time_varying == "dev") {
     # add the time varying feature into the ctl file	 
     # find the line specifying natural mortality params
     ch3 <- grep("# NatM_p_1_Fem_GP_1", SS_ctl)
@@ -134,7 +172,8 @@ change_m <- function(
       val = val[check==FALSE]	# only keep the ones that are non NAs
       ifelse(length(val)==14, print("Everything is fine"), print("Something is wrong. Please check the control.ss.new file"))					
     }	
-    # find the line specifying natural mortality params and specify the environmental link
+    # find the line specifying natural mortality params and specify
+    # the environmental link
     val[9] <- 2
     val[10] = year.beg
     val[11] = year.end
@@ -142,7 +181,7 @@ change_m <- function(
     SS_ctl[ch3] <- paste(c(val, "# NatM_p_1_Fem_GP_1"), collapse=" ")
     # Now specify the custom MG-env setup		
     writeLines(SS_ctl, con= ctl_file_out)
-
+    
     # add grwoth dev estimates into the par file	 
     SS_par <- readLines(con = par_file)
     ch1 <- grep("# SR_parm[1]:", SS_par, fixed=TRUE)
@@ -155,31 +194,68 @@ change_m <- function(
     new.par=c(beg.file, to.add, end.file)
     writeLines(new.par, con = par_file)
   }
+
+  # We are not currently using how_time_varying = "block" and this section may
+  # need updating!!!
+  if(how_time_varying == "block") {
+    # change the block pattern
+    ch1 <- grep("#_Nblock_Patterns", SS_ctl)
+    ch2 <- regexpr("#_Nblock_Patterns", SS_ctl[ch1])[1]
+    val1 <- as.numeric(as.vector(substr(SS_ctl[ch1], start=1, stop=ch2-1)))
+    if (val1>1) print("Beware! There are more than ONE block pattern in this ctl file")
+    SS_ctl[ch1] = paste(1, "#_Nblock_Patterns", sep=" ")
+    SS_ctl[ch1+1] = paste(n_blocks, "#_blocks_per_pattern ", sep=" ")
+    SS_ctl[ch1+3] = paste(block_pattern, collapse=" ")
+    # now add the time varying feature into the model   
+    # find the line specifying natural mortality params
+    ch3 <- grep("# NatM_p_1_Fem_GP_1", SS_ctl)
+    ch4 <- regexpr("# NatM_p_1_Fem_GP_1", SS_ctl[ch3])[1]
+    val = strsplit(substr(SS_ctl[ch3], start=1, stop=ch4-1), " ")[[1]]  # extract the element of the character vector
+    val = as.numeric(val)	# make them a numeric vector
+    check = (is.na(val))
+    if (sum(check)>0) 
+    {
+      print("Some values are removed. Normally it is the white space but checking it...")
+      val = val[check==FALSE]	# only keep the ones that are non NAs
+      ifelse(length(val)==14, print("Everything is fine"), print("Something is wrong. Please check the control.ss.new file"))					
+    }	
+    
+    # find the line specifying natural mortality params and specify
+    # the environmental link
+    val[8] <- -1
+    SS_ctl[ch3] <- paste(c(val, "# NatM_p_1_Fem_GP_1"), collapse=" ")
+    # Now specify the custom MG-env setup
+    ch5 <- grep("#_Cond 0  #custom_MG-env_setup (0/1)", SS_ctl, fixed=TRUE)
+    ch6 <- regexpr(" #custom_MG-env_setup (0/1)", SS_ctl[ch5], fixed=TRUE)[1]
+    SS_ctl[ch5] <- paste(0, "#custom_MG-env_setup (0/1)")
+    val2 <- c(-1, 2, 1, 0, -1, 99, -2)
+    SS_ctl[ch5+1] <- paste(c(val2, " # env link specification i.e fixed to 1"), collapse=" ")
+  }
+  
 }
 
 ## choose the directory to work in 
-   #dir_folder <- "C:\\Users\\Kot\\Dropbox\\Kot working folder\\Fish600\\"
-   #dir_folder <- "C:\\Users\\Kotaro Ono\\Dropbox\\Kot working folder\\Fish600\\"
-   #working_folder <- paste(dir_folder, "trial", sep="")
+#dir_folder <- "C:\\Users\\Kot\\Dropbox\\Kot working folder\\Fish600\\"
+#dir_folder <- "C:\\Users\\Kotaro Ono\\Dropbox\\Kot working folder\\Fish600\\"
+#working_folder <- paste(dir_folder, "trial", sep="")
 ## to do some checking of the model convergence
-   #m1 <- SS_output(dir=working_folder, model="ss3", covar=FALSE, forecast=FALSE)
+#m1 <- SS_output(dir=working_folder, model="ss3", covar=FALSE, forecast=FALSE)
 ## To read in the bootstrap data file and create an R object to modify afterwards
-   #SS_dat <- SS_readdat(file= paste(working_folder, "/YTF.dat", sep=""), verbose = TRUE, echoall = FALSE, section = NULL)
+#SS_dat <- SS_readdat(file= paste(working_folder, "/YTF.dat", sep=""), verbose = TRUE, echoall = FALSE, section = NULL)
 ## To read in the control file and create an R object to modify afterwards
-   #SS_ctl <- readLines(con = paste(working_folder, "/control.ss_new", sep=""))
+#SS_ctl <- readLines(con = paste(working_folder, "/control.ss_new", sep=""))
 ## To read in the par file
-   #SS_par <- scan(file=paste(working_folder, "/ss3.par", sep=""), what="character")
+#SS_par <- scan(file=paste(working_folder, "/ss3.par", sep=""), what="character")
 
-	# SS_ctl[1:50]
-		
-	# # extract the starting/ending year in the .dat file 
-		# year.beg=SS_dat$styr
-		# year.end=SS_dat$endyr
-	# # specifying the different blockpattern (only used for the operating model)
-		# block_pattern = c(year.beg, year.beg+50, year.beg+51, year.end)
-	# # Block value
-		# Env = c(rep(0, 50), rep(0.5, 50))
+# SS_ctl[1:50]
 
+# # extract the starting/ending year in the .dat file 
+# year.beg=SS_dat$styr
+# year.end=SS_dat$endyr
+# # specifying the different blockpattern (only used for the operating model)
+# block_pattern = c(year.beg, year.beg+50, year.beg+51, year.end)
+# # Block value
+# Env = c(rep(0, 50), rep(0.5, 50))
 
 ### Add some time varying param using the env link
 ## block patterm
