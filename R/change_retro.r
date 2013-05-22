601cb361ad55a0ad9dcc0fac6bb52c26d54fd39b
Sean Anderson <github@seananderson.ca>
2013-05-22 15:15:26 -0700

    Re-write change_retro

--- R/change_retro.r
+++ R/change_retro.r
@@ -4,17 +4,26 @@
 #' @param startfile_out Output \code{starter.ss} file
 #' @param retro_yr Which retrospective year to enter into the starter
 #' file. Should be 0 (no retrospective analysis) or a negative value.
-#' @references This function consists of three lines of code extracted
-#' from Ian Taylor's \code{r4ss::SS_doRetro} function by Sean
-#' Anderson.
+#' @author Sean C. Anderson
 #' @export
 
 change_retro <- function(startfile_in = "starter.ss", startfile_out =
   "starter.ss", retro_yr = 0) {
-  starter <- r4ss::SS_readstarter(startfile_in, verbose = FALSE)
-  starter$retro_yr <- retro_yr
-  r4ss::SS_writestarter(starter, file = startfile_out, verbose =
-    FALSE, overwrite = TRUE)
+
+  # Sanity checks:
+  if(retro_yr > 0) 
+    stop("retro_yr should be <= 0")
+  if(abs(retro_yr - round(retro_yr)) > .Machine$double.eps^0.5) 
+    stop("retro_yr should be a whole number or integer")
+
+  starter <- readLines(startfile_in)
+
+  starter_line <- grep("retrospective", starter)
+  retro_dat <- strsplit(starter[starter_line], " ")[[1]]
+  retro_dat[1] <- retro_yr
+  starter[starter_line] <- paste(retro_dat, collapse = " ")
+
+  writeLines(starter, startfile_out)
 }
 
 
