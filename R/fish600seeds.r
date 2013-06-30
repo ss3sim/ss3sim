53046e4f9463f2e0f18ebb25359e2ba0b7d2a890
Sean Anderson <github@seananderson.ca>
2013-06-30 14:08:10 -0700

    Switch to sample() for generating seed values

--- R/fish600seeds.r
+++ R/fish600seeds.r
@@ -2,7 +2,7 @@
 #'
 #' These data are randomly generated seed values.
 #' They were generated with:
-#' \code{set.seed(999);fish600seeds <- round(runif(100, max = 1e6))}
+#' \code{set.seed(999);fish600seeds <- sample(1:1e6, 100, replace = FALSE)}
 #'
 #' @name fish600seeds
 #' @docType data
