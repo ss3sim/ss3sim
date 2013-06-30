0d431311ce02c5424d7c90521b2843baea7e1a27
Sean Anderson <github@seananderson.ca>
2013-06-30 13:27:47 -0700

    Add fish600seeds data

---
+++ R/fish600seeds.r
@@ -0,0 +1,12 @@
+#' 100 randomly generated seeds 
+#'
+#' These data are randomly generated seed values.
+#' They were generated with:
+#' \code{set.seed(999);fish600seeds <- round(runif(100, max = 1e6))}
+#'
+#' @name fish600seeds
+#' @docType data
+#' @keywords data
+NULL
+
+
