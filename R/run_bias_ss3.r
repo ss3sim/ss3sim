709fd025511d6c5e481cd9ccc9d18092d2bb8940
fhurtado <fhurtado@uw.edu>
2013-07-04 14:55:12 -0700

    Add option to change convergence criteria

--- R/run_bias_ss3.r
+++ R/run_bias_ss3.r
@@ -53,7 +53,7 @@
 #' @seealso \code{\link{run_ss3sim}}, \code{\link{run_ss3model}},
 #' \code{\link{run_bias_ss3}}
 
-run_bias_ss3 <-function(dir, outdir, nsim) {
+run_bias_ss3 <-function(dir, outdir, nsim, conv.crit = 0.2) {
   outfile = "AdjustBias.DAT"
   mysims = 1:nsim
   sapply(mysims, bias_ss3, dir = dir)
@@ -85,7 +85,10 @@ run_bias_ss3 <-function(dir, outdir, nsim) {
     row.names = FALSE, col.names = TRUE, quote = FALSE, append = F)
 
   #If the number of NAs (i.e not invertible hessian) > 80% of the cases, then create a WARNING file
-  if(sum(as.numeric(is.na(bias.table$bias1)))/length(bias.table$bias1) > 0.2) { WARNINGS = paste("WARNINGS: more than 20% of cases produces non invertible hessian. These are iterations ", paste(which(is.na(bias.table$bias1)), collapse=",")); write.table(WARNINGS, file=paste0(dir, "/WARNINGS.txt")); stop()}
+  if(sum(as.numeric(is.na(bias.table$bias1)))/length(bias.table$bias1) > conv.crit) 
+    { WARNINGS = paste("WARNINGS: more than 20% of cases produces non invertible hessian. These are iterations ", 
+    paste(which(is.na(bias.table$bias1)), collapse=",")); write.table(WARNINGS, file=paste0(dir, "/WARNINGS.txt")); 
+    stop()}
 
 # Open the control.ss_new file from one of the bias adjustment runs,
   # find where bias adjustment parameters are specified,
