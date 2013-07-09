a59c719cd2b8e3b643dcda2a1e54fd8e883c8730
Sean Anderson <github@seananderson.ca>
2013-07-09 13:10:11 -0700

    Expand seeds to 1000 iterations

--- R/fish600seeds.r
+++ R/fish600seeds.r
@@ -1,8 +1,14 @@
-#' 100 randomly generated seeds 
+#' Randomly generated seeds 
 #'
 #' These data are randomly generated seed values.
 #' They were generated with:
-#' \code{set.seed(999);fish600seeds <- sample(1:1e6, 100, replace = FALSE)}
+#' \code{
+#' set.seed(999)
+#' fish600seeds <- sample(1:1e6, 100, replace = FALSE)
+#' fish600seeds <- c(fish600seeds, sample(1:1e6, 900, replace =
+#' FALSE))
+#' }
+#' I have checked to ensure there are no duplicate seeds.
 #'
 #' @name fish600seeds
 #' @docType data
