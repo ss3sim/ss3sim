5ab0557b96538d959fdd3e879de84dddea0f01c2
Sean Anderson <github@seananderson.ca>
2013-08-08 20:18:55 -0300

    Add stochastic survey R code for ms example

--- inst/examples/survey-cv.r
+++ inst/examples/survey-cv.r
@@ -11,27 +11,51 @@ dx0 <- get_caseargs("cases", "D0-E100-F0-G0-R0-S0-M0-cod")
 dx1 <- get_caseargs("cases", "D0-E100-F0-G0-R0-S0-M0-cod") 
 recdevs_det <- matrix(0.001, nrow=100, ncol=20)
 
+# deterministic cases:
 with(dx0, run_ss3sim(
-    iterations = 20, 
-    scenarios = "D0-E100-F0-G0-R0-S0-M0-cod", 
-    bias_adjust = TRUE,
-    m_params = M, f_params = F, index_params = index, 
-    lcomp_params = lcomp, agecomp_params = agecomp, 
-    estim_params = E, 
-    om_model_dir = "../extdata/models/cod-om/", 
-    em_model_dir = "../extdata/models/cod-em/",
-    sleep = 20,
-    user_recdevs = recdevs_det))
+  iterations = 20, 
+  scenarios = "D0-E100-F0-G0-R0-S0-M0-cod", 
+  bias_adjust = TRUE,
+  m_params = M, f_params = F, index_params = index, 
+  lcomp_params = lcomp, agecomp_params = agecomp, 
+  estim_params = E, 
+  om_model_dir = "../extdata/models/cod-om/", 
+  em_model_dir = "../extdata/models/cod-em/",
+  sleep = 20,
+  user_recdevs = recdevs_det))
 
 with(dx1, run_ss3sim(
-    iterations = 20, 
-    scenarios = "D1-E100-F0-G0-R0-S0-M0-cod", 
-    bias_adjust = TRUE,
-    m_params = M, f_params = F, index_params = index, 
-    lcomp_params = lcomp, agecomp_params = agecomp, 
-    estim_params = E, 
-    om_model_dir = "../extdata/models/cod-om/", 
-    em_model_dir = "../extdata/models/cod-em/",
-    sleep = 20,
-    user_recdevs = recdevs_det))
+  iterations = 20, 
+  scenarios = "D1-E100-F0-G0-R0-S0-M0-cod", 
+  bias_adjust = TRUE,
+  m_params = M, f_params = F, index_params = index, 
+  lcomp_params = lcomp, agecomp_params = agecomp, 
+  estim_params = E, 
+  om_model_dir = "../extdata/models/cod-om/", 
+  em_model_dir = "../extdata/models/cod-em/",
+  sleep = 20,
+  user_recdevs = recdevs_det))
+
+# stochastic cases:
+with(x0, run_ss3sim(
+  iterations = 100, 
+  scenarios = "D0-E0-F0-G0-R0-S0-M0-cod", 
+  bias_adjust = TRUE,
+  m_params = M, f_params = F, index_params = index, 
+  lcomp_params = lcomp, agecomp_params = agecomp, 
+  estim_params = E, 
+  om_model_dir = "../extdata/models/cod-om/", 
+  em_model_dir = "../extdata/models/cod-em/",
+  sleep = 30))
+
+with(x1, run_ss3sim(
+  iterations = 100, 
+  scenarios = "D1-E0-F0-G0-R0-S0-M0-cod", 
+  bias_adjust = TRUE,
+  m_params = M, f_params = F, index_params = index, 
+  lcomp_params = lcomp, agecomp_params = agecomp, 
+  estim_params = E, 
+  om_model_dir = "../extdata/models/cod-om/", 
+  em_model_dir = "../extdata/models/cod-em/",
+  sleep = 30))
 
