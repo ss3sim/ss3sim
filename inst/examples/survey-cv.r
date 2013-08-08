6b90c9ad762abb646c633d6d84203436052970b0
Sean Anderson <github@seananderson.ca>
2013-08-08 20:14:05 -0300

    Update ms survey example to run deterministic cases

--- inst/examples/survey-cv.r
+++ inst/examples/survey-cv.r
@@ -1,11 +1,37 @@
 # Look at the benefit of increasing survey effort to halve survey CV
 
+# set the working directory to *this* directory, for example:
+# setwd("~/src/ss3sim/inst/examples/")
+
 library(ss3sim)
 
-setwd("~/src/ss3sim/inst/examples/")
+x0 <- get_caseargs("cases", "D0-E0-F0-G0-R0-S0-M0-cod")
+x1 <- get_caseargs("cases", "D0-E0-F0-G0-R0-S0-M0-cod")
+dx0 <- get_caseargs("cases", "D0-E100-F0-G0-R0-S0-M0-cod") 
+dx1 <- get_caseargs("cases", "D0-E100-F0-G0-R0-S0-M0-cod") 
+recdevs_det <- matrix(0.001, nrow=100, ncol=20)
 
-cases <- get_caseargs(folder = "cases", scenario = "D0-E0-F0-G0-R0-S0-M0-cod")
+with(dx0, run_ss3sim(
+    iterations = 20, 
+    scenarios = "D0-E100-F0-G0-R0-S0-M0-cod", 
+    bias_adjust = TRUE,
+    m_params = M, f_params = F, index_params = index, 
+    lcomp_params = lcomp, agecomp_params = agecomp, 
+    estim_params = E, 
+    om_model_dir = "../extdata/models/cod-om/", 
+    em_model_dir = "../extdata/models/cod-em/",
+    sleep = 20,
+    user_recdevs = recdevs_det))
 
-with(cases, run_ss3sim(iterations = 1, scenarios = "D0-E0-F0-G0-R0-S0-M0-cod", m_params = M, f_params = F, index_params = index, lcomp_params = lcomp, agecomp_params = agecomp, estim_params = E, om_model_dir = "../extdata/models/cod-om/", em_model_dir = "../extdata/models/cod-em/"))
-
+with(dx1, run_ss3sim(
+    iterations = 20, 
+    scenarios = "D1-E100-F0-G0-R0-S0-M0-cod", 
+    bias_adjust = TRUE,
+    m_params = M, f_params = F, index_params = index, 
+    lcomp_params = lcomp, agecomp_params = agecomp, 
+    estim_params = E, 
+    om_model_dir = "../extdata/models/cod-om/", 
+    em_model_dir = "../extdata/models/cod-em/",
+    sleep = 20,
+    user_recdevs = recdevs_det))
 
