b3e3304830ea21b2b78634f76dda45f24f77ba22
Sean Anderson <github@seananderson.ca>
2013-08-10 14:00:13 -0700

    Add example output data

---
+++ R/final_results_scalar.r
@@ -0,0 +1,12 @@
+#' Example scalar data output
+#'
+#' These data represent the output from a simple example in which the
+#' observation error on a survey is manipulated. Case \code{D0} refers
+#' to the base case and case \code{D1} refers to a case in which the
+#' standard deviation on the survey observation error is halved.
+#'
+#' @name final_results_scalar.r
+#' @docType data
+#' @keywords data
+NULL
+
---
+++ R/final_results_ts.r
@@ -0,0 +1,17 @@
+#' Example time series data output
+#'
+#' These data represent the output from a simple example in which the
+#' observation error on a survey is manipulated. Case \code{D0} refers
+#' to the base case and case \code{D1} refers to a case in which the
+#' standard deviation on the survey observation error is halved.
+#' 
+#' Since the file size of these time series output data can be large,
+#' we have reduced the output to 15 iterations.
+#'
+#' @name final_results_ts
+#' @docType data
+#' @keywords data
+NULL
+
+
+
