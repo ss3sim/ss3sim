6314336ae1abea1925621c5012763518abed866b
Sean Anderson <github@seananderson.ca>
2013-08-10 22:14:46 -0300

    Prepare example for cluster

--- inst/examples/1-run-survey-cv-m-cross.r
+++ inst/examples/1-run-survey-cv-m-cross.r
@@ -9,16 +9,22 @@ case_folder <- paste0(d, "/eg-cases")
 om <- paste0(d, "/models/cod-om")
 em <- paste0(d, "/models/cod-em")
 
-run_fish600(iterations = 1, scenarios = c("D0-E0-F0-G0-R0-S0-M0-cod",
-    "D1-E0-F0-G0-R0-S0-M0-cod", "D0-E1-F0-G0-R0-S0-M0-cod",
-    "D1-E1-F0-G0-R0-S0-M0-cod"), case_folder = case_folder,
-  om_model_dir = om, em_model_dir = em)
+# deterministic:
+run_fish600(iterations = 1:20, scenarios = 
+  #c("D0-E0-F0-G0-R0-S0-M0-cod",
+    #"D1-E0-F0-G0-R0-S0-M0-cod", 
+    c("D0-E1-F0-G0-R0-S0-M0-cod",
+    "D1-E1-F0-G0-R0-S0-M0-cod"), 
+  case_folder = case_folder, om_model_dir = om, em_model_dir = em,
+  bias_adjust = TRUE)
 
     
-run_fish600(iterations = 1, scenarios = c("D0-E100-F0-G0-R0-S0-M0-cod",
-    "D1-E100-F0-G0-R0-S0-M0-cod", "D0-E101-F0-G0-R0-S0-M0-cod",
-    "D1-E101-F0-G0-R0-S0-M0-cod"), case_folder = case_folder,
-  om_model_dir = om, em_model_dir = em)
-
-
+# stochastic:
+run_fish600(iterations = 1:100, scenarios = 
+  #c("D0-E100-F0-G0-R0-S0-M0-cod",
+    #"D1-E100-F0-G0-R0-S0-M0-cod", 
+    c("D0-E101-F0-G0-R0-S0-M0-cod",
+    "D1-E101-F0-G0-R0-S0-M0-cod"), 
+  case_folder = case_folder, om_model_dir = om, em_model_dir = em,
+  bias_adjust = TRUE)
 
