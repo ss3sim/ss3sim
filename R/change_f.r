bf3e101d2c06aa43318b83eff80770e11f6ebe5b
Sean Anderson <github@seananderson.ca>
2013-09-05 19:21:41 -0700

    Touch up change_f and code

--- R/change_f.r
+++ R/change_f.r
@@ -1,11 +1,13 @@
-#' Alter the Fvalues in an ss3.par file
+#' Alter the fishing mortality (F) values in an \code{ss3.par} file
+#'
+#' Takes an SS3 \code{.par} file and changes the F values for
+#' specified years.
 #'
 #' @author Curry James Cunningham
-#' @details Allows for specific years in series to be altered
 #'
-#' @param years Vector of years for which Fvalues are specified
-#' @param years_alter Vector of years for which Fvalue will be altered
-#' @param fvals Vector of Fvalues to be entered into ss3.par file
+#' @param years Vector of years for which F values are specified
+#' @param years_alter Vector of years for the which F values will be altered
+#' @param fvals Vector of F values to be entered into \code{ss3.par} file
 #' @param file_in Input SS3 par file.
 #' @param file_out Output SS3 par file. 
 #' @examples 
@@ -17,32 +19,30 @@
 #' # clean up
 #' file.remove("test.par")
 #' }
-#'
 #' @export
 
 change_f <- function(years, years_alter, fvals, file_in="ss3.par", file_out="ss3.par") {
 	
   n.years_alter <- length(years_alter)
-  #Check that sufficient Fvalues are supplied
+
+  # Check that sufficient F values are supplied
   if(n.years_alter != length(fvals)) { 
     stop(paste('#ERROR: Number of years to alter:', n.years_alter,
         'Does NOT equal length of supplied Fvalues:', length(fvals))) 
   }
   
-  #Read in ss3.par file
-  ss3.par <- readLines(file_in) #Original 
-  ss3.par.new <- ss3.par #New file
+  # Read in ss3.par file
+  ss3.par <- readLines(file_in) # Original 
+  ss3.par.new <- ss3.par # New file
   
   for(y in 1:n.years_alter) {
   	temp.year <- which(years == years_alter[y])
-    temp.loc <- which(ss3.par == paste('# F_rate[', temp.year, ']:', sep='')) #Temporary location
+    temp.loc <- which(ss3.par == paste('# F_rate[', temp.year, ']:', sep='')) # Temporary location
     ss3.par.new[temp.loc+1] <- fvals[y]
-  }#next y
+  } # next y
   
-  #Write new .par file
+  # Write new .par file
   writeLines(ss3.par.new, con=file_out)
   close(file(file_out))
   invisible(ss3.par.new)
 }
-
-
