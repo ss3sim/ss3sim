74ba3c7c2eb5a057b1627f37e502c4d457454f39
Sean Anderson <github@seananderson.ca>
2013-09-30 10:22:46 -0700

    Shrink recdev.rda file to reduce space. Now 125x500.

--- R/recdevs.r
+++ R/recdevs.r
@@ -7,8 +7,8 @@
 #'
 #' The columns are intended to be for iterations and the rows for
 #' years. The matrix has intentionally constructed at a large size to
-#' accommodate studies that work with simulations up to 150 years
-#' long and with up to 1000 iterations. If you want to work with more
+#' accommodate studies that work with simulations up to 125 years
+#' long and with up to 500 iterations. If you want to work with more
 #' iterations or longer time series then you'll need to specify your
 #' own recdevs through the \code{user_recdevs} argument in
 #' \code{\link{ss3sim_base}}. The function
@@ -18,10 +18,15 @@
 #' \dontrun{
 #' recdevs <- matrix(nrow = 150, ncol = 1000)
 #' set.seed(999)
-#' recdevs[1:100,   1:100] <- matrix(nrow = 100, ncol = 100, data = rnorm(100*100))
-#' recdevs[1:100, 101:500] <- matrix(nrow = 100, ncol = 400, data = rnorm(100*400))
-#' recdevs[101:150, 1:500] <- matrix(nrow = 50,  ncol = 500, data = rnorm(50*500))
-#' recdevs[1:150, 501:1000] <- matrix(nrow = 150, ncol = 500, data = rnorm(150*500))
+#' recdevs[1:100,   1:100] <- matrix(nrow = 100, ncol = 100, data =
+#'   rnorm(100*100))
+#' recdevs[1:100, 101:500] <- matrix(nrow = 100, ncol = 400, data =
+#'   rnorm(100*400))
+#' recdevs[101:150, 1:500] <- matrix(nrow = 50,  ncol = 500, data =
+#'   rnorm(50*500))
+#' recdevs[1:150, 501:1000] <- matrix(nrow = 150, ncol = 500, data =
+#'   rnorm(150*500))
+#' recdevs <- recdevs[1:125, 1:500]
 #' save(recdevs, file = "../data/recdevs.rda")
 #' }
 #'
