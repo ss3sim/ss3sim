202cb5eb615215a06c1c8a641d363e6c05cb8473
Sean Anderson <github@seananderson.ca>
2013-10-06 15:22:36 -0700

    Add missing parallel test folder objects

--- inst/examples/test-parallel.r
+++ inst/examples/test-parallel.r
@@ -4,6 +4,11 @@ require(foreach)
 registerDoParallel(cores = 2)
 getDoParWorkers()
 
+d <- system.file("extdata", package = "ss3sim")
+case_folder <- paste0(d, "/eg-cases")
+om <- paste0(d, "/models/cod-om")
+em <- paste0(d, "/models/cod-em")
+
 time_par <- system.time({
 run_ss3sim(iterations = 1:20, scenarios = c("D0-E0-F0-G0-R0-S0-M0-cod",
     "D1-E0-F0-G0-R0-S0-M0-cod"), case_folder = case_folder,
