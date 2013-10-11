45d18e7d9f136de67f254db01ec841fc99ac4f1a
Sean Anderson <github@seananderson.ca>
2013-10-11 14:13:10 -0700

    Update expand_scenarios to work with arbitrary case IDs

--- R/expand_scenarios.r
+++ R/expand_scenarios.r
@@ -1,35 +1,37 @@
-#' Create vectors of scenarios from inputs. 
+#' Create vectors of scenario IDs
 #'
 #' Create vectors of scenarios from inputs. For passing to
-#' \code{\link{run_ss3sim}}, \code{\link{run_fish600}}, or
-#' \code{\link{get_results_all}}. Default case values are the base
-#' case (\code{0}).
+#' \code{\link{run_ss3sim}}, or \code{\link{get_results_all}}. Default
+#' case values are the base case (\code{0}).
 #'
-#' @param e Integer values of estimation cases to be expanded
-#' @param d Integer values of data cases to be expanded
-#' @param f Integer values of fishing mortality cases to be expanded
-#' @param g Integer values of growth cases to be expanded
-#' @param m Integer values of natural mortality cases to be expanded
-#' @param r Integer values of retrospective cases to be expanded
-#' @param s Integer values of selectivity cases to be expanded
-#' @param species Vector of characters in \code{c("cod","fla","sar")}
-#' @author Cole Monnahan
+#' @param cases A named list of cases. The names in the list are the
+#' case IDs and the values are the case values.
+#' @param species Vector of 3-letter character IDs designating the
+#' species/stock.
+#' @author Cole Monnahan and Sean Anderson
 #' @export
+#' @seealso \code{\link{run_ss3sim}}, \code{\link{get_results_all}}
+#' @return A character vector of scenario IDs. The case IDs will be
+#' alphabetically sorted.
 #' @examples
 #' expand_scenarios()
-#' expand_scenarios(f=0:2, species="cod")
+#' expand_scenarios(cases = list(D = 0:3, E = 0, F = 0, M = 0, R = 0),
+#' species = "cod")
 
-expand_scenarios <- function(e = 0, d = 0, f = 0, g = 0, m = 0, 
-  r = 0, s = 0, species = c("cod", "fla", "sar")) {
-  e <- paste0("E", e)
-  d <- paste0("D", d)
-  f <- paste0("F", f)
-  g <- paste0("G", g)
-  m <- paste0("M", m)
-  r <- paste0("R", r)
-  s <- paste0("S", s)
-  df <- as.data.frame(expand.grid(e, d, f, g, m, r, s,  
-    species, stringsAsFactors = FALSE))
+expand_scenarios <- function(cases = list(D = 0, E = 0, F = 0,
+    M = 0, R = 0), species = c("cod", "fla", "sar")) {
+
+  cases <- cases[order(names(cases))]
+  sapply(species, function(x) if(nchar(x) != 3) 
+    stop("species ID must be 3 characters"))
+
+  cases_all <- c(cases, list(species))
+  case_names <- names(cases_all)
+  cases_paste <- list()
+  for(i in 1:length(cases_all)) {
+    cases_paste[[i]] <- paste0(case_names[i], cases_all[[i]])
+  }
+  df <- expand.grid(cases_paste, stringsAsFactors = FALSE)
   scenarios <- apply(df, 1, paste, collapse = "-")
   return(scenarios)
 }
