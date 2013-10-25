6aa7b92868c23dc9d4e2350cae5d0b0c30f97c41
Sean Anderson <github@seananderson.ca>
2013-10-24 19:16:57 -0700

    Add "tangled" R code from vignette to run on server

---
+++ vignettes/ss3sim-vignette.R
@@ -0,0 +1,44 @@
+library(ss3sim)
+d <- system.file("extdata", package = "ss3sim")
+case_folder <- paste0(d, "/eg-cases")
+om <- paste0(d, "/models/cod-om")
+em <- paste0(d, "/models/cod-em")
+
+recdevs_det <- matrix(0, nrow = 100, ncol = 20)
+
+require(doParallel)
+registerDoParallel(cores = 4)
+require(foreach)
+# getDoParWorkers()
+
+run_ss3sim(iterations = 1:20, scenarios =
+  c("D0-E100-F0-R0-M0-cod",
+    "D1-E100-F0-R0-M0-cod",
+    "D0-E101-F0-R0-M0-cod",
+    "D1-E101-F0-R0-M0-cod"),
+  case_folder = case_folder, om_model_dir = om, em_model_dir = em,
+  bias_adjust = TRUE, user_recdevs = recdevs_det, parallel = TRUE)
+
+run_ss3sim(iterations = 1:50, scenarios =
+  c("D0-E0-F0-R0-M0-cod",
+    "D1-E0-F0-R0-M0-cod",
+    "D0-E1-F0-R0-M0-cod",
+    "D1-E1-F0-R0-M0-cod"),
+  case_folder = case_folder, om_model_dir = om,
+  em_model_dir = em, bias_adjust = TRUE, parallel = TRUE)
+
+get_results_all(user_scenarios =
+  c("D0-E100-F0-R0-M0-cod",
+    "D1-E100-F0-R0-M0-cod",
+    "D0-E101-F0-R0-M0-cod",
+    "D1-E101-F0-R0-M0-cod",
+    "D0-E0-F0-R0-M0-cod",
+    "D1-E0-F0-R0-M0-cod",
+    "D0-E1-F0-R0-M0-cod",
+    "D1-E1-F0-R0-M0-cod"))
+
+scalar_dat <- read.csv("final_results_scalar.csv")
+ts_dat <- read.csv("final_results_ts.csv")
+
+save(ts_dat, file = "ts_dat.rda")
+save(scalar_dat, file = "scalar_dat.rda")
