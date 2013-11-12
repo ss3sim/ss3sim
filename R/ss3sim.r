d36fa33c1f2adcad8ea470708528c1df715614eb
Sean Anderson <github@seananderson.ca>
2013-11-11 21:56:35 -0800

    Strip whitespace and replace R with \R in ss3sim.r

--- R/ss3sim.r
+++ R/ss3sim.r
@@ -1,7 +1,7 @@
-#' ss3sim: An R package for fisheries stock assessment simulation with
+#' ss3sim: An \R package for fisheries stock assessment simulation with
 #' Stock Synthesis
 #'
-#' The ss3sim R package is designed to facilitate large-scale, rapid,
+#' The ss3sim \R package is designed to facilitate large-scale, rapid,
 #' and reproducible simulation with the widely-used Stock Synthesis 3
 #' (SS3) statistical catch-at-age stock assessment framework.
 #'
@@ -16,7 +16,7 @@
 #' model of how you will assess that truth (an SS3 estimation model),
 #' (3) and a set of cases that deviate from these base models that you
 #' want to compare (configuration arguments provided as plain-text
-#' control files). 
+#' control files).
 #'
 #' You can find examples of these SS3 operating and estimation models
 #' within the package data (\code{inst/extdata/models/}). The package
@@ -24,7 +24,7 @@
 #' \code{inst/extdata/cases} and \code{inst/extdata/eg-cases}.
 #'
 #' The main ss3sim functions are divided into three types:
-#' 
+#'
 #' 1. \code{change} functions that manipulate SS configuration files.
 #' These manipulations generate an underlying "truth" (operating
 #' models) and control our assessment of those models (estimation
@@ -32,30 +32,30 @@
 
 #' \itemize{
 #' \item \code{\link{change_f}}: Controls fishing mortality.
-#' 
+#'
 #' \item \code{\link{change_tv}}: Adds time-varying features. For
 #' example, time-varying natural mortality, growth, or selectivity.
-#' 
+#'
 #' \item \code{\link{change_lcomp}}: Controls how length composition
 #' data are sampled.
-#' 
+#'
 #' \item \code{\link{change_agecomp}}: Controls how age composition
 #' data are sampled.
-#' 
+#'
 #' \item \code{\link{change_index}}: Controls how the fishery and
 #' survey indices are sampled.
-#' 
+#'
 #' \item \code{\link{change_e}}: Controls which and how parameters are
 #' estimated.
-#' 
+#'
 #' \item \code{\link{change_retro}}: Controls the number of years to
 #' discard for a retrospective analysis.
-#' 
+#'
 #' \item \code{\link{change_rec_devs}}: Substitutes recruitment
 #' deviations.
-#' 
+#'
 #' }
-#' 
+#'
 #' 2. \code{run} functions that conduct simulations. These functions
 #' generate a folder structure, call manipulation functions, run `SS3`
 #' as needed, and save the output.
@@ -66,19 +66,19 @@
 #' \item \code{\link{ss3sim_base}}: Underlying base simulation
 #' function. Can also be called directly.
 #' }
-#' 
-#' 
+#'
+#'
 #' 3. \code{get} functions for synthesizing the output.
 #' \itemize{
 #' \item \code{\link{get_results_scenario}}: Extract the results for a
 #' single scenario.
-#' 
+#'
 #' \item \code{\link{get_results_all}}: Extract results from a series
 #' of scenarios.
 #' }
-#' 
+#'
 #' See the package vignette \code{vignette("ss3sim-vignette")} for
-#' more extensive explanation of how to use the ss3sim R package.
+#' more extensive explanation of how to use the ss3sim \R package.
 #'
 #' @docType package
 #' @name ss3sim
