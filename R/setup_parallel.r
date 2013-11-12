2c2ce8ee87ff7ad7ccfeca23744b014209d6d13f
Sean Anderson <github@seananderson.ca>
2013-11-12 10:59:47 -0800

    Clean up setup_parallel.r

--- R/setup_parallel.r
+++ R/setup_parallel.r
@@ -1,3 +1,4 @@
+#' Setup parallel processing
 setup_parallel <- function() {
   if(!require("foreach")) {
     stop("The foreach package is required for parallel scenario simulations.",
@@ -5,11 +6,11 @@ setup_parallel <- function() {
   }
   cores <- getDoParWorkers()
   if(cores == 1) {
-    warning("You have only registered one core. Reverting to
-non-parallel processing. You need to register multiple cores
-first. For example:
+    warning(
+"You have only registered one core. Reverting to non-parallel
+processing. You need to register multiple cores first. For example:
 library(doParallel)
-registerDoParallel(cores = 3)
+registerDoParallel(cores = 2)
 See the help for ?run_ss3sim for an example.")
   }
   cores
