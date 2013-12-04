9e69bcaa0794d8cd37dbe0857c8b76247b97aea1
Sean Anderson <github@seananderson.ca>
2013-12-04 11:40:03 -0800

    Touch up docs on user_recdevs and add warning

--- R/ss3sim_base.r
+++ R/ss3sim_base.r
@@ -28,14 +28,15 @@
 #'   and use for the specified simulations.
 #' @param user_recdevs An optional matrix of recruitment deviations to replace
 #'   the recruitment deviations built into the package. The columns represent
-#'   run iterations and the rows represent years. Note that these will be
-#'   multiplied by sigma R. So, under normal conditions this matrix should
-#'   contain standard-normal values \code{~N(-0.5, 1)}. The mean of -0.5 is so
-#'   the deviations will be mean unbiased after exponentiation. This could
-#'   also be a matrix of 0s for deterministic model checking. Note that user
-#'   specified recruitment deviations are not bias adjusted (by subtracting
-#'   half the variance from the mean), but the built in default recruitment
-#'   deviations are.
+#'   run iterations and the rows represent years. \code{user_recdevs} can be a
+#'   matrix of 0s for deterministic model checking. For traditional stochastic
+#'   simulations these would be independent and normally distributed deviations
+#'   with a standard deviation equal to the desired sigma R. Note that these
+#'   recruitment deviations will be used verbatim (after exponentiation).
+#'   \code{user_recdevs} will *not* be multiplied by sigma R and they will *not*
+#'   be log-normal bias corrected. If \code{user_recdevs} are specified as
+#'   anything besides \code{NULL} the package will issue a warning about this.
+#'   Biased recruitment deviations can lead to biased model results.
 #' @param bias_adjust Run bias adjustment first? See \code{\link{run_bias_ss3}}.
 #' @param bias_nsim If bias adjustment is run, how many simulations should the
 #'   bias adjustment factor be estimated from? It will take the mean of the
@@ -178,6 +179,10 @@ ss3sim_base <- function(iterations, scenarios, f_params,
       if(is.null(user_recdevs)) {
         sc_i_recdevs <- sigmar * recdevs - sigmar^2/2 # from the package data
       } else {
+        warning(
+"No bias correction is done internally for user-supplied recruitment deviations
+and must be done manually. See the vignette for more details. Biased recruitment
+deviations can lead to biased model results.")
         sc_i_recdevs <- user_recdevs[, this_run_num] # user specified recdevs
       }
 
