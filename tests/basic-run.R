ed127264d2795491f01d89ea7a8935f15fa227d4
Sean Anderson <github@seananderson.ca>
2014-07-22 10:46:14 -0700

    Switch basic-run test to work in working directory
    Easier to debug

--- tests/basic-run.R
+++ tests/basic-run.R
@@ -1,8 +1,8 @@
 # From ?run_ss3sim:
-temp_path <- file.path(tempdir(), "ss3sim-example")
-dir.create(temp_path, showWarnings = FALSE)
-wd <- getwd()
-setwd(temp_path)
+#temp_path <- file.path(tempdir(), "ss3sim-example")
+#dir.create(temp_path, showWarnings = FALSE)
+#wd <- getwd()
+#setwd(temp_path)
 
 # Find the data in the ss3sim package:
 d <- system.file("extdata", package = "ss3sim")
@@ -12,6 +12,6 @@ case_folder <- paste0(d, "/eg-cases")
 
 # Without bias adjustment:
 run_ss3sim(iterations = 1:1, scenarios = "D0-E0-F0-R0-M0-cod",
-  case_folder = case_folder, om_dir = om, em_dir = em)
-setwd(wd)
-unlink(temp_path, recursive = TRUE) # clean up
+  case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized")
+#setwd(wd)
+unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
