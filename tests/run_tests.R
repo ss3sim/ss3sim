fa67b3d00c4c8c72245fce26fa7a9d8a6b0beb00
colemonnahan <monnahc@uw.edu>
2014-08-29 14:30:17 -0700

    updated testing code for tail compression to be stand alone

---
+++ tests/run_tests.R
@@ -0,0 +1,8 @@
+## This is a central file from which to source the different testing
+## scripts.
+
+## Make sure wd is set to this testing folder first.
+devtools::load_all('..')
+
+source("test_tail_compression.R")
+
--- tests/test_tail_compression.R
+++ tests/test_tail_compression.R
@@ -7,14 +7,14 @@
 ## it, adapt it, or get ideas from it. -Cole 8/28/2014.
 
 ## Test whether cases are parsed correctly
-stop("Not ready to be sourced, see code comments for reasons")
+## stop("Not ready to be sourced, see code comments for reasons")
 get_caseargs("cases", scenario = "D0-E0-F0-M0-R0-S0-T0-cod",
              case_files = list(E = "E", D = c("index", "lcomp", "agecomp"), F =
-             "F", M = "M", R = "R", S = "S", T="T"))
+             "F", M = "M", R = "R", S = "S", T="T"))$T
 
 ## Run the example simulation with tail compression option
 case_folder <- 'cases'
-d <- system.file("extdata", package = "ss3sim")
+d <- '../inst/extdata'
 om <- paste0(d, "/models/cod-om")
 em <- paste0(d, "/models/cod-em")
 run_ss3sim(iterations = 1, scenarios =
@@ -31,16 +31,12 @@ run_ss3sim(iterations = 1, scenarios =
 get_results_all(user_scenarios=
                 c("D0-E0-F0-R0-M0-T0-cod",
                   "D0-E0-F0-R0-M0-T1-cod",
-                  "D0-E0-F0-R0-M0-cod" ), over=TRUE)
+                  "D0-E0-F0-R0-M0-cod" ))
 results <- read.csv("ss3sim_scalar.csv")
-results$ID <- gsub("D0-E0-F0-R0-M0-|-1", "", as.character(results$ID))
-results.long <- cbind(ID=results$ID, results[,grep("_em", names(results))])
-results.long <- reshape2::melt(results.long, "ID")
-library(ggplot2)
-ggplot(results.long, aes(x=ID, y=value))+
-    geom_point()+facet_wrap("variable", scales="free")
+print("The first two values should be the same, the third different:")
+print( results$NLL_em[c(1,3,2)])
+
 ## End of session so clean up
-results.long
 file.remove("ss3sim_scalar.csv", "ss3sim_ts.csv")
 unlink("D0-E0-F0-R0-M0-T0-cod", TRUE)
 unlink("D0-E0-F0-R0-M0-T1-cod", TRUE)
