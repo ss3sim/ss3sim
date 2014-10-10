66d5539d99180d470185b49c71f0d9c2ae6433c7
Sean Anderson <github@seananderson.ca>
2014-10-10 11:01:02 -0700

    Tweak basic-run.R test

--- tests/basic-run.R
+++ tests/basic-run.R
@@ -15,17 +15,17 @@ om <- paste0(d, "/models/cod-om")
 em <- paste0(d, "/models/cod-em")
 case_folder <- paste0(d, "/eg-cases")
 
+## Basic run
+# serial:
+run_ss3sim(iterations = 1, scenarios = "D0-E0-F0-R0-M0-cod",
+  case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "safe")
+unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
+
 # Set parallel cores:
 library(doParallel)
 library(foreach)
 registerDoParallel(cores = 2)
 getDoParWorkers() # check
-
-## Basic run
-# serial:
-run_ss3sim(iterations = 1:1, scenarios = "D0-E0-F0-R0-M0-cod",
-  case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "safe")
-unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
 
 # parallel:
 run_ss3sim(iterations = 1:1,
