6da14bf169e4f6427b2ea7d908852c88169d7052
Sean Anderson <github@seananderson.ca>
2014-10-28 22:00:13 -0700

    Trivial in-progress work on testing script

--- tests/test-change-bin.R
+++ tests/test-change-bin.R
@@ -4,8 +4,9 @@
 d <- '../inst/extdata'
 om <- paste0(d, "/models/cod-om")
 em <- paste0(d, "/models/cod-em")
-case_folder <- paste0(d, "/cases")
+case_folder <- "cases"
 
 run_ss3sim(iterations = 1, scenarios = "D2-E0-F0-R0-M0-cod",
   case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "safe")
 unlink("D2-E0-F0-R0-M0-cod", recursive = TRUE)
+#unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE)
