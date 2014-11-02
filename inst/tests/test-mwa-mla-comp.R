b08de291adf009a4e3d0b196938d322cfe7f20d9
Sean Anderson <github@seananderson.ca>
2014-11-02 01:35:07 -0700

    Add test for sample_mwacomp()

--- inst/tests/test-mwa-mla-comp.R
+++ inst/tests/test-mwa-mla-comp.R
@@ -30,4 +30,27 @@ test_that("sample_mlacomp() works", {
   unlink("codOM-temp.dat")
 })
 
+test_that("sample_mwacomp() works", {
+  change_bin(fdat, file_out = "codOM-temp.dat",
+    type = c("mwa"),
+    fleet_dat = list("mwa" = list(years = list(2000:2012), fleets = 1)),
+    write_file = TRUE)
+  set.seed(123)
+  out <- sample_mwacomp("codOM-temp.dat", outfile = "ignore.dat", ctlfile = fctl,
+    Nsamp = list(rep(50, 13)), years = list(2000:2012), write_file = FALSE,
+    mean_outfile = NULL)
+  expect_equal(names(out),
+    c("Yr", "Seas", "Fleet", "Gender", "Part", "AgeErr", "Ignore",
+      "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10",
+      "a11", "a12", "a13", "a14", "a15", "N_a1", "N_a2", "N_a3", "N_a4",
+      "N_a5", "N_a6", "N_a7", "N_a8", "N_a9", "N_a10", "N_a11", "N_a12",
+      "N_a13", "N_a14", "N_a15"))
+  expect_equal(round(out$a1, 2),
+    c(0.9, 0.99, 1, 0.96, 0.97, 1.09, 0.96, 0.99, 1.05, 0.99, 0.96,
+      0.87, 1.12))
+  expect_equal(out$N_a1,
+    c(2, 4, 4, 3, 5, 3, 6, 3, 5, 4, 5, 1, 2))
+  unlink("codOM-temp.dat")
+})
+
 setwd(wd)
