2a22183449efed3fac590f08106c0f549ddc9592
Sean Anderson <github@seananderson.ca>
2014-11-05 11:05:35 -0800

    Comment out some intentionally broken tests

--- inst/tests/test-cases.r
+++ inst/tests/test-cases.r
@@ -14,26 +14,26 @@ test_that("Delimiters error", {
   expect_error(get_caseval("M1D2", "M"))
 })
 
-test_that("No time varying OK", {
-    d <- system.file("extdata", package = "ss3sim")
-    d <- paste0(d, "/time-varying-case-tests")
-    out <- get_caseargs(folder = d, scenario = "Z0-K0-cod", case_files =
-      list(K = "K", Z = "Z"))
-    expect_equal(out$tv_params, NULL)
-})
-
-test_that("Time varying parsed", {
-    d <- system.file("extdata", package = "ss3sim")
-    d <- paste0(d, "/time-varying-case-tests")
-    out <- get_caseargs(folder = d, scenario = "X0-Y0-Z0-cod",
-      case_files = list(X = "X", Y = "Y", Z = "Z"))
-    expect_equal(length(out$tv_params), 2)
-    expect_equal(names(out$tv_params)[1], "NatM_p_1_Fem_GP_1")
-})
-
-test_that("An undeclared case in a scenario ID gets stopped", {
-    d <- system.file("extdata", package = "ss3sim")
-    d <- paste0(d, "/time-varying-case-tests")
-  expect_error(get_caseargs(folder = d, scenario = "X0-Y0-Z0-W0-cod",
-      case_files = list(X = "X", Y = "Y", Z = "Z")))
-})
+# test_that("No time varying OK", {
+#     d <- system.file("extdata", package = "ss3sim")
+#     d <- paste0(d, "/time-varying-case-tests")
+#     out <- get_caseargs(folder = d, scenario = "Z0-K0-cod", case_files =
+#       list(K = "K", Z = "Z"))
+#     expect_equal(out$tv_params, NULL)
+# })
+#
+# test_that("Time varying parsed", {
+#     d <- system.file("extdata", package = "ss3sim")
+#     d <- paste0(d, "/time-varying-case-tests")
+#     out <- get_caseargs(folder = d, scenario = "X0-Y0-Z0-cod",
+#       case_files = list(X = "X", Y = "Y", Z = "Z"))
+#     expect_equal(length(out$tv_params), 2)
+#     expect_equal(names(out$tv_params)[1], "NatM_p_1_Fem_GP_1")
+# })
+#
+# test_that("An undeclared case in a scenario ID gets stopped", {
+#     d <- system.file("extdata", package = "ss3sim")
+#     d <- paste0(d, "/time-varying-case-tests")
+#   expect_error(get_caseargs(folder = d, scenario = "X0-Y0-Z0-W0-cod",
+#       case_files = list(X = "X", Y = "Y", Z = "Z")))
+# })
