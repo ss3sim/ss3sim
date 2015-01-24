9454b72bae01ff45a6bfec59c81af70e472ef5ee
Sean Anderson <github@seananderson.ca>
2015-01-24 17:00:57 -0500

    Tweaks to change_year()

--- R/change_year.R
+++ R/change_year.R
@@ -1,30 +1,29 @@
 #' Methods to change the years in an ss3sim model
 #'
-#' \code{change_years} takes SS3 \code{.ctl}, \code{.dat},  
-#' \code{.par}, \code{starter.ss}, and \code{.forecast} files
-#' and changes the start and end year of the model.
-#' \code{change_years} is set up to work with \code{ss3sim}
-#' operating model or estimation model.
+#' \code{change_years} takes SS3 \code{.ctl}, \code{.dat}, \code{.par},
+#' \code{starter.ss}, and \code{.forecast} files and changes the start and end
+#' year of the model. \code{change_years} works with \pkg{ss3sim} operating or
+#' estimation models.
 #'
 #' @param year_begin Desired start year
 #' @param year_end Desired end year
-#' @param burnin Length of burnin period. Default is zero for an OM.
-#' Use burnin for EM models, to establish a period with no fishing.
+#' @param burnin Length of burnin period. Default is zero for an OM. Use burnin
+#'   for EM models, to establish a period with no fishing.
 #' @param ctl_file_in Input SS3 control file
-#' @param ctl_file_out Output SS3 control file, if NULL the file will
-#' be named the same as the \code{ctl_file_in}
+#' @param ctl_file_out Output SS3 control file, if \code{NULL} the file will be
+#'   named the same as the \code{ctl_file_in}
 #' @param dat_file_in Input SS3 data file
-#' @param dat_file_out Output SS3 data file, if NULL the file will
-#' be named the same as the \code{dat_file_in}
+#' @param dat_file_out Output SS3 data file, if \code{NULL} the file will be
+#'   named the same as the \code{dat_file_in}
 #' @param par_file_in Input SS3 parameter file
-#' @param par_file_out Output SS3 parameter file, if NULL the file will
-#' be named the same as the \code{par_file_in}
+#' @param par_file_out Output SS3 parameter file, if \code{NULL} the file will
+#'   be named the same as the \code{par_file_in}
 #' @param starter_file_in Input SS3 starter file
-#' @param starter_file_out Output SS3 starter file, if NULL the file will
-#' be named the same as the \code{starter_file_in}
+#' @param starter_file_out Output SS3 starter file, if \code{NULL} the file will
+#'   be named the same as the \code{starter_file_in}
 #' @param forecast_file_in Input SS3 forecast file
-#' @param forecast_file_out Output SS3 forecast file, if NULL the file will
-#' be named the same as the \code{forecast_file_in}
+#' @param forecast_file_out Output SS3 forecast file, if \code{NULL} the file
+#'   will be named the same as the \code{forecast_file_in}
 #' @author Kelli Johnson
 #' @return The function creates modified versions of the \code{.par},
 #'   \code{.dat} , \code{.ctl}, \code{.starter}, and \code{.forecast} files.
@@ -36,11 +35,13 @@
 #' if the data type is present in the \code{.dat} file.
 #' Manipulation done to the \code{.dat} file is not complete and users will need
 #' to specify data for years which are deleted. The function removes all composition
-#' data except for the first year. 
+#' data except for the first year.
 #' To remove data use \code{\link{sample_index}}, \code{\link{sample_lcomp}},
-#' or \code{\link{sample_agecomp}}. 
+#' or \code{\link{sample_agecomp}}.
 #' For models that use the \code{.forecast} file, all references to years
 #' must be made with relative values (i.e., 0 or negative integers).
+#'
+#' @importFrom r4ss SS_readforecast SS_readdat SS_writeforecast
 #'
 #' @export
 #'
@@ -82,7 +83,7 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
   starter_file_in = NULL, starter_file_out = "starter.ss",
   forecast_file_in = NULL, forecast_file_out = "forecast.ss",
   verbose = FALSE) {
-  
+
   if (is.null(ctl_file_out)) {
     ctl_file_out <- ctl_file_in
   }
@@ -103,8 +104,7 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
   years.use <- year_begin:year_end
   if (burnin > 0) {
     if (length(years.use) <= burnin) {
-      stop("Burnin period is greater than or equal to\n
-           total number of years.")
+      stop("Burnin period is greater than or equal to total number of years.")
     }
     years.use <- years.use[-c(1:burnin)]
   }
@@ -117,17 +117,17 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
     input.split[1] <- new
     output <- paste(input.split, collapse = " #")
     file[line] <- output
-    return(file)
+    file
   }
 
   # Work with starter file
   if (!is.null(starter_file_in)) {
     ss3.starter <- readLines(con = starter_file_in)
-    ss3.starter <- manipulate(ss3.starter, "min yr", -1) 
-    ss3.starter <- manipulate(ss3.starter, "max yr", -2) 
+    ss3.starter <- manipulate(ss3.starter, "min yr", -1)
+    ss3.starter <- manipulate(ss3.starter, "max yr", -2)
     writeLines(ss3.starter, con = starter_file_out)
   }
-  
+
   # Work with ctl file
   if (!is.null(ctl_file_in)) {
     ss3.ctl <- readLines(con = ctl_file_in)
@@ -142,13 +142,13 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
                              "#")[[1]]
     ballpark.val.1 <- as.numeric(sub(" ", "", ballpark.val[1]))
     if (ballpark.val.1 > 0) {
-      stop("Currently the function only works if \n
-            F ballpark year in the .ctl file is negative.")
+      stop(paste0("Currently the function only works if F ballpark year",
+                  "in the .ctl file is negative."))
     } else{
       ss3.ctl <- manipulate(ss3.ctl, "F ballpark year", year_end * -1)
     }
     fmethod <- strsplit(grep("F_Method", ss3.ctl, value = TRUE), "")[[1]]
-    fmethod <- fmethod[-which(fmethod == " ")][1]    
+    fmethod <- fmethod[-which(fmethod == " ")][1]
     if (fmethod == 2) {
         F_value.start <- grep("F_value", ss3.ctl) + 1
         F_value.end <- grep("_initial_F_parms", ss3.ctl) - 1
@@ -185,33 +185,31 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
     # Work with F values in par file
     F.words <- grep("F", ss3.par, value = TRUE)
       if (any(grepl("Fcast_recruitments", F.words))) {
-        warning("right now the code does not work with\n
-                Fcast_recruitments")
+        warning("right now the code does not work with Fcast_recruitments")
       }
       if (any(grepl("Fcast_impl_error", F.words))) {
-        warning("#right now the code does not work with\n
-                Fcast_impl_error")
+        warning("right now the code does not work with Fcast_impl_error")
       }
       if (any(grepl("F_rate", F.words))) {
             F.line <- grep("F_rate", ss3.par)
             F.init.line <- grep("init_F", ss3.par)
             F.template <- c(ss3.par[F.line[1]], 0.0)
             # Remove old F's
-            ss3.par <- ss3.par[-seq(F.line[1], 
+            ss3.par <- ss3.par[-seq(F.line[1],
                                     tail(F.line, 1) + 1, by = 1)]
             counter <- F.init.line + 1
             for (f in seq(year_span)) {
-              ss3.par <- append(ss3.par, F.template, 
+              ss3.par <- append(ss3.par, F.template,
                                 after = counter)
               counter <- counter + 2
             }
       }
     writeLines(ss3.par, con = par_file_out)
   }
-  
+
   # Work with dat file
   if (!is.null(dat_file_in)) {
-    ss3.dat <- r4ss::SS_readdat(dat_file_in, verbose = verbose, 
+    ss3.dat <- SS_readdat(dat_file_in, verbose = verbose,
                                 echoall = FALSE, section = NULL)
     ss3.dat$styr <- year_begin
     ss3.dat$endyr <- year_end
@@ -222,11 +220,11 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
     dim.fleets <- dim(data.catch)[2] - which(names(data.catch) == "year")
     dim.data <- c(length(data.catch.seasons) * length(years.use), dim.fleets + 2)
     data.catch.new <- matrix(NA, nrow = dim.data[1], ncol = dim.data[2])
-  
+
     counter <- 1
     for(q in seq_along(years.use)) {
       for(s in seq_along(data.catch.seasons)) {
-        data.catch.new[counter, ] <- c(rep(0, dim.fleets), years.use[q], 
+        data.catch.new[counter, ] <- c(rep(0, dim.fleets), years.use[q],
                                        data.catch.seasons[s])
         counter <- counter + 1
       }
@@ -245,10 +243,10 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
     for(s in seq_along(data.cpue.seasons)) {
       for(i in seq_along(data.cpue.index)) {
         for(y in seq_along(years.use)) {
-          se.value <- subset(data.cpue, index == data.cpue.index[i] & 
+          se.value <- subset(data.cpue, index == data.cpue.index[i] &
                              seas == data.cpue.seasons[s],
                              select = "se_log")[1, 1]
-          data.cpue.new[counter, ] <- c(years.use[y], data.cpue.seasons[s], 
+          data.cpue.new[counter, ] <- c(years.use[y], data.cpue.seasons[s],
                                         data.cpue.index[i], 1, se.value)
           counter <- counter + 1
         }
@@ -264,11 +262,11 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
       data.discard <- ss3.dat$discard_data
       data.discard.new <- expand.grid(years.use,
                                       unique(data.discard$Seas),
-                                      unique(data.discard$Flt), 
+                                      unique(data.discard$Flt),
                                       0, 0)
       colnames(data.discard.new) <- colnames(data.discard)
       ss3.dat$discard_data <- data.discard.new
-      ss3.dat$N_discard <- dim(data.discard.new)[1] 
+      ss3.dat$N_discard <- dim(data.discard.new)[1]
     }
     # Change meanbodyweight if it exists
     n.meanbdwt <- ss3.dat$N_meanbodywt
@@ -304,17 +302,18 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
     if (num.env > 0) {
         data.env <- ss3.dat$envdat
         data.env.variables <- unique(data.env$Variable)
-        data.env.new <- data.frame("Yr" = rep(seq(year_begin, year_end), 
+        data.env.new <- data.frame("Yr" = rep(seq(year_begin, year_end),
                                               num.env),
-                                   "Variable" = rep(data.env.variables, each = year_span),
+                                   "Variable" = rep(data.env.variables,
+                                     each = year_span),
                                    "Value" = rep(0, num.env * year_span))
         ss3.dat$envdat <- data.env.new
     }
     # size frequency
         if (any(grepl("# N sizefreq methods to read", ss3.dat))) {
-            stop("change_year does not accomodate dat files with
-                  sizefreq methods. Please remove the sizefreq
-                  data and run change_year again.")
+            stop(paste0("change_year does not accomodate dat files with",
+                  "sizefreq methods. Please remove the sizefreq",
+                  "data and run change_year again."))
         }
     SS_writedat(ss3.dat, dat_file_out, overwrite = TRUE, verbose = verbose)
   }
@@ -335,15 +334,15 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
       realyears <- strsplit(strsplit(realyears, "# ")[[1]][2], " ")[[1]]
       realyears <- as.numeric(realyears[!realyears == ""])
     if (all(forecast$Bmark_years == realyears)) {
-      warning("Forecast biomass and fishing benchmark years\n
-              must be set relative to the end year and not as\n
-              absolute years. All benchmark years were set to \n
-              the terminal year. Please manually change")
+      warning(paste0("Forecast biomass and fishing benchmark years",
+              "must be set relative to the end year and not as",
+              "absolute years. All benchmark years were set to",
+              "the terminal year. Please manually change"))
       forecast$Bmark_years <- rep(0, 6)
       forecast$Fcast_years <- rep(0, 4)
     }
     if (forecast$FirstYear_for_caps_and_allocations != 0) {
-        forecast$FirstYear_for_caps_and_allocations <- year_end + 
+        forecast$FirstYear_for_caps_and_allocations <- year_end +
           (forecast$FirstYear_for_caps_and_allocations - max(realyears))
         }
     if (forecast$Ydecl != 0) {
@@ -357,19 +356,18 @@ change_year <- function(year_begin = 1, year_end = 100, burnin = 0,
     }
     if (all(forecast$max_totalcatch_by_area == 0)) {
       forecast$max_totalcatch_by_area <- "#"
-    }    
+    }
     if (all(forecast$fleet_assignment_to_allocation_group == 0)) {
       forecast$fleet_assignment_to_allocation_group <- "#"
     }
 
-    if (forecast$fleet_relative_F != 1 | 
+    if (forecast$fleet_relative_F != 1 |
         forecast$Ncatch != 0) {
-      stop("change_year is not set up do change the relative F\n
-            or the catch per year. This must be done manually,\n
-            or change to use first-last-allocation year.")
+      stop(paste0("change_year is not set up do change the relative F",
+            "or the catch per year. This must be done manually,",
+            "or change to use first-last-allocation year."))
     }
     SS_writeforecast(forecast, file = forecast_file_out,
                      overwrite = TRUE, verbose = verbose)
   }
-  }
-
+}
