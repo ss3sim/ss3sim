f220c7d276613174f0be7ee4b72d48d95395550c
Sean Anderson <github@seananderson.ca>
2015-01-24 20:32:14 -0500

    Tweak run_ss3sim unit tests

--- tests/testthat/test-run-ss3sim.R
+++ tests/testthat/test-run-ss3sim.R
@@ -1,5 +1,8 @@
 context("run_ss3sim and get-results functions work across a range of scenarios")
 
+# Note that these tests are skipped on CRAN since the SS3 executable won't
+# be available (and some of these take a while to run).
+
 temp_path <- file.path(tempdir(), "ss3sim-test")
 dir.create(temp_path, showWarnings = FALSE)
 wd <- getwd()
@@ -11,31 +14,25 @@ em <- paste0(d, "/models/cod-em")
 case_folder <- paste0(d, "/eg-cases")
 
 test_that("A basic run_ss3sim scenario runs", {
-
   skip_on_cran()
-
   run_ss3sim(iterations = 1, scenarios = "D0-E0-F0-R0-M0-cod",
     case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized")
   unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
 })
 
 test_that("run_ss3sim works with parallel iterations", {
-
   skip_on_cran()
-
   library("doParallel")
   library("foreach")
   registerDoParallel(cores = 2)
   run_ss3sim(iterations = 1:2, scenarios = "D0-E0-F0-R0-M0-cod",
     case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized",
     parallel = TRUE, parallel_iterations = TRUE)
-  unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
+  unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE)
 })
 
 test_that("run_ss3sim works with parallel scenarios", {
-
   skip_on_cran()
-
   run_ss3sim(iterations = 1,
     scenarios = c("D0-E0-F0-R0-M0-cod", "D0-E0-F0-R1-M0-cod"),
     case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized",
@@ -43,22 +40,28 @@ test_that("run_ss3sim works with parallel scenarios", {
 })
 
 test_that("get_results_all is working", {
-
   skip_on_cran()
-
   get_results_all()
   d <- read.csv("ss3sim_scalar.csv")
   d <- read.csv("ss3sim_ts.csv")
   unlink(c("ss3sim_scalar.csv", "ss3sim_ts.csv"))
+})
 
+test_that("get_results_all is working in parallel", {
+  skip_on_cran()
   get_results_all(parallel = TRUE)
   d <- read.csv("ss3sim_scalar.csv")
   d <- read.csv("ss3sim_ts.csv")
   unlink(c("ss3sim_scalar.csv", "ss3sim_ts.csv"))
+})
 
-  # last one doesn't exist:
+test_that("get_results_all warns if a scenario folder isn't available", {
+  skip_on_cran()
   expect_warning(
     get_results_all(user_scenarios =
         c("D0-E0-F0-R0-M0-cod", "D1-E0-F0-R1-M0-cod", "D0-E0-F0-R0-M0-X0-cod")))
   unlink(c("ss3sim_scalar.csv", "ss3sim_ts.csv"))
+
+  # done with these scenarios now:
+  unlink(c("D0-E0-F0-R0-M0-cod", "D0-E0-F0-R1-M0-cod"), recursive = TRUE)
 })
