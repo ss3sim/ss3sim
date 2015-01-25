506289653d2ff58f7e01d76a943e93c12de911c6
Sean Anderson <github@seananderson.ca>
2015-01-24 20:13:56 -0500

    Add get_results_all unit tests

--- tests/testthat/test-run-ss3sim.R
+++ tests/testthat/test-run-ss3sim.R
@@ -1,4 +1,4 @@
-context("run_ss3sim works across a range of scenarios")
+context("run_ss3sim and get-results functions work across a range of scenarios")
 
 temp_path <- file.path(tempdir(), "ss3sim-test")
 dir.create(temp_path, showWarnings = FALSE)
@@ -11,14 +11,18 @@ em <- paste0(d, "/models/cod-em")
 case_folder <- paste0(d, "/eg-cases")
 
 test_that("A basic run_ss3sim scenario runs:", {
+
   skip_on_cran()
+
   run_ss3sim(iterations = 1, scenarios = "D0-E0-F0-R0-M0-cod",
     case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized")
   unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
 })
 
 test_that("run_ss3sim works with parallel iterations:", {
+
   skip_on_cran()
+
   library("doParallel")
   library("foreach")
   registerDoParallel(cores = 2)
@@ -29,13 +33,32 @@ test_that("run_ss3sim works with parallel iterations:", {
 })
 
 test_that("run_ss3sim works with parallel scenarios:", {
+
   skip_on_cran()
+
   run_ss3sim(iterations = 1,
     scenarios = c("D0-E0-F0-R0-M0-cod", "D0-E0-F0-R1-M0-cod"),
     case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized",
     parallel = TRUE)
-  unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE)
-  unlink("D1-E0-F0-R0-M0-cod", recursive = TRUE)
 })
 
-setwd(wd)
+test_that("get_results_all is working", {
+
+  skip_on_cran()
+
+  get_results_all()
+  d <- read.csv("ss3sim_scalar.csv")
+  d <- read.csv("ss3sim_ts.csv")
+  unlink(c("ss3sim_scalar.csv", "ss3sim_ts.csv"))
+
+  get_results_all(parallel = TRUE)
+  d <- read.csv("ss3sim_scalar.csv")
+  d <- read.csv("ss3sim_ts.csv")
+  unlink(c("ss3sim_scalar.csv", "ss3sim_ts.csv"))
+
+  # last one doesn't exist:
+  expect_warning(
+    get_results_all(user_scenarios =
+        c("D0-E0-F0-R0-M0-cod", "D1-E0-F0-R1-M0-cod", "D0-E0-F0-R0-M0-X0-cod")))
+  unlink(c("ss3sim_scalar.csv", "ss3sim_ts.csv"))
+})
