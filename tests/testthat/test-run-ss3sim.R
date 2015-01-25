ee1bdcd9317a21e5a2d0837655a2168d9b384748
Sean Anderson <github@seananderson.ca>
2015-01-24 19:08:42 -0500

    Start adding run_ss3sim unit tests

---
+++ tests/testthat/test-run-ss3sim.R
@@ -0,0 +1,28 @@
+context("run_ss3sim works across a range of scenarios")
+
+temp_path <- file.path(tempdir(), "ss3sim-test")
+dir.create(temp_path, showWarnings = FALSE)
+wd <- getwd()
+setwd(temp_path)
+
+d <- system.file("extdata", package = "ss3sim")
+om <- paste0(d, "/models/cod-om")
+em <- paste0(d, "/models/cod-em")
+case_folder <- paste0(d, "/eg-cases")
+
+test_that("A basic run_ss3sim scenario runs:", {
+  skip_on_cran()
+  run_ss3sim(iterations = 1, scenarios = "D0-E0-F0-R0-M0-cod",
+    case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized")
+  unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
+})
+
+test_that("run_ss3sim works with parallel iterations:", {
+  skip_on_cran()
+  run_ss3sim(iterations = 1:2, scenarios = "D0-E0-F0-R0-M0-cod",
+    case_folder = case_folder, om_dir = om, em_dir = em, ss_mode = "optimized",
+    parallel = TRUE, parallel_iterations = TRUE)
+  unlink("D0-E0-F0-R0-M0-cod", recursive = TRUE) # clean up
+})
+
+setwd(wd)
