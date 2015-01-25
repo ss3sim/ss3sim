cdb1153811c666719073b364b0a4498a1e5dfc5c
Sean Anderson <github@seananderson.ca>
2015-01-24 22:05:53 -0500

    Re-wrap change_tail_compression comments

--- R/change_tail_compression.R
+++ R/change_tail_compression.R
@@ -1,22 +1,21 @@
 #' Replace tail compression value for length composition data.
 #'
-#' This function replaces the tail compression value for length composition
-#' data in a \code{dat} file (\code{file_in}) with those specified in
-#' \code{tail_compression}. It then writes a new file with name
-#' \code{file_out} into the working directory. If used with
-#' \code{\link{run_ss3sim}} the case file should be named
-#' \code{tail_compression}. A suggested case letter is \code{T}.
+#' This function replaces the tail compression value for length composition data
+#' in a \code{dat} file (\code{file_in}) with those specified in
+#' \code{tail_compression}. It then writes a new file with name \code{file_out}
+#' into the working directory. If used with \code{\link{run_ss3sim}} the case
+#' file should be named \code{tail_compression}. A suggested case letter is
+#' \code{T}.
 #'
-#' @param tail_compression *The new tail_compression value to be used. Must
-#' be a numeric value, as a proportion. For example 0.1 means 10
-#' percent. See the SS3 manual for further information. A NULL value
-#' indicates no action, a negative value indicates to SS3 to ignore it (not
-#' use that feature).
-#' @param datfile Input SS3 dat file \bold{as returned in list object from
-#'   from \code{\link[r4ss]{SS_readdat}}}.
+#' @param tail_compression *The new tail_compression value to be used. Must be a
+#'   numeric value, as a proportion. For example 0.1 means 10 percent. See the
+#'   SS3 manual for further information. A NULL value indicates no action, a
+#'   negative value indicates to SS3 to ignore it (not use that feature).
+#' @param datfile Input SS3 dat file \bold{as returned in list object form from
+#'   \code{\link[r4ss]{SS_readdat}}}.
 #' @param file_out Output SS3 dat file name.
-#' @return A modified SS3 \code{.dat} file, and that file returned
-#' invisibly (for testing) as a vector of character lines.
+#' @return A modified SS3 \code{.dat} file, and that file returned invisibly
+#'   (for testing) as a vector of character lines.
 #' @template casefile-footnote
 #' @author Cole Monnahan
 #' @export
@@ -33,7 +32,7 @@
 #' test <- change_tail_compression(tail_compression = .1234, input_dat,
 #'  file_out = paste0(temp_path, "/test.dat"))
 #' ## Look at the changes
-#' test$comp_tail_compression
+#' print(test$comp_tail_compression)
 #' ## Clean up the temp files
 #' unlink(temp_path)
 
