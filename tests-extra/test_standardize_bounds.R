f416260a9c77024babb4cf7b06c1f29dd944bcad
kellijohnson <kellifayejohnson@gmail.com>
2015-02-04 20:18:50 -0800

    Deleted local test and created a testthat
    Not sure if the test will work with CRAN testthat because
    you need the most uptodate version of r4ss.

--- tests-extra/test_standardize_bounds.R
+++ tests-extra/test_standardize_bounds.R
@@ -1,23 +1,46 @@
-require("r4ss")
-#Set to the models folder
-model.path<-"C:\\Users\\Christine\\Documents\\GitHub\\growth_models\\"
+# Not sure if the below test will work within the package build b/c
+# of the use of the newest version of r4ss
 
-#Path and filename of OM and EM control file
-OM.ctl<-"fla-om\\flaOM.ctl"
-EM.ctl<-"fla-em\\flaEM.ctl"
+context("Test standardize_bounds() with latest r4ss")
 
-#Use SS_parlines to get the proper names for parameters for the data frame
-om.pars<-SS_parlines(ctlfile=paste0(model.path,OM.ctl))
-em.pars<-SS_parlines(ctlfile=paste0(model.path,EM.ctl))
+library(devtools)
+install_github("r4ss/r4ss", ref = "master")
+library(r4ss)
+library(ss3sim)
 
-#From the Johnson et al paper
-lo.percent<-c(rep(.5,7),rep(-20,3))
-hi.percent<-c(500,1000,1000,rep(500,4),rep(20,3))
+temp_path <- file.path(tempdir(), "ss3sim-test")
+dir.create(temp_path, showWarnings = FALSE)
+wd <- getwd()
+setwd(temp_path)
 
-#Populate data frame using EM parameter names and percentages from the Johnson et al paper
-#Indices are the parameters you want to modify
-percent.df<-data.frame(Label=as.character(em.pars[c(1:6,17,24:26),"Label"]),lo=lo.percent,hi=hi.percent)
+## Set to the path and filename of the OM and EM control files
+file.copy(system.file("extdata", "models", "cod-em", package = "ss3sim"),
+          ".", recursive = TRUE)
+ctlom <- system.file("extdata", "models", "cod-om", "codOM.ctl",
+  package = "ss3sim")
 
 
-standardize_bounds(percent_df=percent.df,dir=model.path,em_ctl_file=EM.ctl,om_ctl_file=OM.ctl)
+test_that{"Bounds in EM are changed",
+file.copy(ctlom, "cod-em/om.ctl", overwrite = TRUE)
+  # Use SS_parlines to get the proper names for parameters for the data frame
+  om.pars <- SS_parlines(ctlfile = "cod-em/om.ctl")
+  em.pars <- SS_parlines(ctlfile = "cod-em/codEM.ctl")
+  #From the Johnson et al paper
+  lo.percent<-c(rep(0.5, 7), rep(-20, 3))
+  hi.percent<-c(500, 1000, 1000, rep(500, 4), rep(20, 3))
+  #Populate data frame using EM parameter names and percentages from the
+  # Johnson et al (2015) paper
+  #Indices are the parameters you want to modify
+  parlines <- c(1:6, 17, 24:26)
+  percent.df <- data.frame(
+    Label = as.character(em.pars[parlines, "Label"]),
+    lo = lo.percent, hi = hi.percent)
+  standardize_bounds(percent_df = percent.df, dir = "cod-em",
+                     em_ctl_file = "codEM.ctl", om_ctl_file = "om.ctl")
+  newpars <- SS_parlines(ctlfile = "cod-em/codEM.ctl")[parlines, ]
+  expect_equal(newpars$LO[1:7],
+               em.pars[parlines[1:7], "INIT"] * lo.percent[1:7],
+               tolerance = 0.001)
+}
 
+unlink(temp_path, recursive = TRUE)
