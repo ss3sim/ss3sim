5c1510a161a2eb279b24a5232ede9a24c92c019a
Sean Anderson <sean@seananderson.ca>
2015-02-12 10:55:48 -0500

    Add a more elaborate example to change_em_binning()

--- R/change_em_binning.r
+++ R/change_em_binning.r
@@ -28,9 +28,10 @@
 #' @importFrom magrittr %>%
 #' @importFrom dplyr group_by_ summarise_each_ rename_ mutate_ inner_join funs_
 #' @export
-#' @seealso \code{\link{sample_lcomp}}, \code{\link{sample_agecomp}}
+#' @family sample functions
 #' @family change functions
-#' @author Kotaro Ono
+#' @author Kotaro Ono (length-composition rebinning), Sean Anderson
+#'   (catch-at-length rebinning)
 #' @examples
 #' d <- system.file("extdata", package = "ss3sim")
 #' f_in <- paste0(d, "/example-om/data.ss_new")
@@ -45,16 +46,29 @@
 #' print(l$lbin_vector)
 #' print(head(l$lencomp))
 #'
-#' # An example with catch-at-length re-binning:
+#' # An small example with catch-at-length re-binning:
 #' f <- system.file("extdata", "models", "cod-om", "codOM.dat", package = "ss3sim")
 #' d <- r4ss::SS_readdat(f)
 #' # Add catch at length data (and simplify the bin structure for this example)
-#' datfile <- ss3sim::change_data(d, outfile = NULL, write_file = FALSE,
+#' olddat <- change_data(d, outfile = NULL, write_file = FALSE,
 #'   types = c("len", "age", "cal"), fleets = 1, years = seq(2000, 2002),
 #'   age_bins = 1:3, len_bins = 4:8)
-#' datfile$agecomp
-#' newdat <- change_em_binning(datfile, file_out = NULL, bin_vector = c(4, 6, 8),
+#' olddat$agecomp
+#' newdat <- change_em_binning(olddat, file_out = NULL, bin_vector = c(4, 6, 8),
 #'   lbin_method = 1, rebin_cal = TRUE, write_file = FALSE)
+#' newdat$agecomp
+#'
+#' # A larger catch-at-length re-rebinning example:
+#' olddat <- change_data(d, outfile = NULL, write_file = FALSE,
+#'  types = c("len", "age", "cal"), fleets = 1, years = seq(2000, 2005),
+#'  age_bins = seq(1, 5), len_bins = round(seq(20, 30, length.out = 13), 1))
+#' olddat$lbin_vector
+#' head(olddat$lencomp)
+#' head(olddat$agecomp)
+#' newdat <- change_em_binning(olddat, file_out = NULL, bin_vector = seq(20, 30, 2),
+#'  lbin_method = 1, rebin_cal = TRUE, write_file = FALSE)
+#' newdat$lbin_vector
+#' head(newdat$lencomp)
 #' newdat$agecomp
 
 change_em_binning <- function(datfile, file_out, bin_vector, lbin_method=NULL,
@@ -84,11 +98,11 @@ change_em_binning <- function(datfile, file_out, bin_vector, lbin_method=NULL,
   }
   if (max(bin_vector) > max(datfile$lbin_vector)) {
     stop(paste("the maximum value in the bin_vector is above the original one",
-      "this column will be filled with zero observation so it is meaningless"))
+      "this column would be filled with zero observation so it is meaningless"))
   }
   if (min(bin_vector) < min(datfile$lbin_vector)) {
     stop(paste("the minimum value in the bin_vector is below the original one",
-      "this column will be filled with zero observation so it is meaningless"))
+      "this column would be filled with zero observation so it is meaningless"))
   }
 
   # Find ID columns and data columns to replace:
