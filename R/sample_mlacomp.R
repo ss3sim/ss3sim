7f6984b8dc80a414262729ad4156dea0da8207c0
kellijohnson <kellifayejohnson@gmail.com>
2015-02-14 06:36:11 -0800

    add \code{NULL}

--- R/sample_mlacomp.R
+++ R/sample_mlacomp.R
@@ -16,7 +16,7 @@
 #'   the OM parameters for growth. These values are used to determine the
 #'   uncertainty about size for fish sampled in each age bin.
 #' @param mean_outfile A path to write length and age data for external
-#' estimation of parametric growth. If NULL no file will be written.
+#' estimation of parametric growth. If \code{NULL} no file will be written.
 #' This file is used by \code{change_e} to externally estimate growth
 #' parameters. Filename must contain "vbgf" to be used by \code{change_e}.
 #' Also, if "remove" is included in the filename, the mean length at age data
