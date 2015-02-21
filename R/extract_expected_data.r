ff7d7a6ebb550638b3ae03f3072d0a0e4c3299a2
kellijohnson <kellifayejohnson@gmail.com>
2015-02-20 19:10:06 -0800

    Change extract_expected_data
    With no bootstrap data the function was failing. Now it relies on
    999 to delineate where to stop.

--- R/extract_expected_data.r
+++ R/extract_expected_data.r
@@ -6,15 +6,14 @@
 #' @param data_ss_new The location of the \code{.ss_new} file that was
 #' generated from a run of SS.
 #' @param data_out The location of the \code{.ss_new} file that was
-#' generated from a run of SS.                                                  
+#' generated from a run of SS.
 #' @author Kotaro Ono
 
 extract_expected_data <- function(data_ss_new = "data.ss_new",
   data_out = "ss3.dat") {
   data_file <- readLines(data_ss_new)
   data_file_new <- data_file[(
-    grep("#_expected values with no error added", 
-      data_file, fixed=TRUE)+1):(grep("#_bootstrap file: 1", 
-      data_file, fixed=TRUE)-1)]
+    grep("#_expected values with no error added",
+      data_file, fixed=TRUE)+1):grep("[9]{3}$", data_file)[2]]
     writeLines(data_file_new, con = data_out)
 }
