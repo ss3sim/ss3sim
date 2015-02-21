228f99589f0bce4fa5108e6656615e0c7b122c59
colemonnahan <monnahc@uw.edu>
2015-02-21 11:44:27 -0800

    fixed bug in grep, changed to search for ENDDATA instead of 999 which can exist in the data values

--- R/extract_expected_data.r
+++ R/extract_expected_data.r
@@ -12,8 +12,14 @@
 extract_expected_data <- function(data_ss_new = "data.ss_new",
   data_out = "ss3.dat") {
   data_file <- readLines(data_ss_new)
-  data_file_new <- data_file[(
-    grep("#_expected values with no error added",
-      data_file, fixed=TRUE)+1):grep("[9]{3}$", data_file)[2]]
-    writeLines(data_file_new, con = data_out)
+  x1 <- grep("#_expected values with no error added", data_file, fixed=TRUE)+1
+  x2 <- grep("ENDDATA", data_file)-2
+  if(length(x1)!=1 | length(x2)!=1)
+      stop("grep failed to find lines for extracting data, not of length 1")
+  if(x2<x1)
+      stop("Something wrong with OM data.ss_new file, last line comes before first line")
+  data_file_new <- data_file[x1:x2]
+  if(999 != as.numeric(data_file_new[length(data_file_new)]))
+     warning("last line of extracted expected values was not 999, was it read in correctly?")
+  writeLines(data_file_new, con = data_out)
 }
