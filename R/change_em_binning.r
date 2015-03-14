96d2c524b4f0450c429d5bcbed1ed5b73c8ed281
colemonnahan <monnahc@uw.edu>
2015-03-14 01:00:20 -0700

    added error messages back and instead changed case files; a much better appraoch

--- R/change_em_binning.r
+++ R/change_em_binning.r
@@ -111,14 +111,14 @@ change_em_binning <- function(datfile, file_out, bin_vector, lbin_method = NULL,
     stop(paste("_Ngenders is greater than 1 in the model.",
       "change_em_binning only works with single-gender models."))
   }
-  ## if (!identical(as.integer(max(bin_vector)), as.integer(max(datfile$lbin_vector)))) {
-  ##   stop(paste("The maximum value in the bin_vector is not equal to the",
-  ##     "original maximum length bin value."))
-  ## }
-  ## if(!identical(as.integer(min(bin_vector)), as.integer(min(datfile$lbin_vector)))) {
-  ##   stop(paste("The minimum value in the bin_vector is not equal to the",
-  ##     "original maximum length bin value."))
-  ## }
+  if (!identical(as.integer(max(bin_vector)), as.integer(max(datfile$lbin_vector)))) {
+    stop(paste("The maximum value in the bin_vector is not equal to the",
+      "original maximum length bin value."))
+  }
+  if(!identical(as.integer(min(bin_vector)), as.integer(min(datfile$lbin_vector)))) {
+    stop(paste("The minimum value in the bin_vector is not equal to the",
+      "original maximum length bin value."))
+  }
   if (any(!is_divisible(bin_vector, by_ = datfile$binwidth)) ) {
     stop(paste("One or more of the values in bin_vector are not divisible by",
       "the population binwidth specified in the SS3 data file."))
