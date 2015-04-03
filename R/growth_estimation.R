7ccfadd7a0d2f09b93cd6722ab8bbe22cffef0b4
cstawitz <cstawitz@uw.edu>
2015-04-02 17:46:07 -0700

    Add logistic function to bound growth parameters

--- R/growth_estimation.R
+++ R/growth_estimation.R
@@ -28,19 +28,33 @@ vbgf_func <- function(L1, L.inf, k, ages, a3){
 #' @param a3 integer, the youngest age well sampled in the data.
 #' @param A integer, the oldest age well sampled in the data.
 sample_fit_vbgf <- function(length.data, start.L1, start.L2, start.k,
-  start.cv.young, start.cv.old, a3, A){
+  start.cv.young, start.cv.old, lo.L1, lo.L2, lo.k, lo.cv.young, lo.cv.old,
+  hi.L1, hi.L2, hi.k, hi.cv.young, hi.cv.old, a3, A){
 
+  browser()
   get_vbgf_loglik <- function(logL1, logLinf, logk, logcv.old, logcv.young){
-    L1 <- exp(logL1)
-    L.inf <- exp(logLinf)
-    k <- exp(logk)
-    slope <- (exp(logcv.old) - exp(logcv.young)) / (A - a3)
-    cv <- exp(logcv.young) + slope * (data_$age-a3)  # intercept = cv_young
+    L1 <- inv_logistic(logL1, lo.L1, hi.L1)
+    L.inf <- inv_logistic(logLinf, lo.Linf, hi.Linf)
+    k <- inv_logistic(logk, lo.k, hi.k)
+    cv.young <- inv_logistic(logcv.young, lo.cv.young, hi.cv.young)
+    cv.old <- inv_logistic(logcv.old, lo.cv.old, hi.cv.old)
+    slope <- (cv.old - cv.young) / (A - a3)
+    cv <- cv.young + slope * (data_$age-a3)  # intercept = cv_young
     cv <- ifelse(cv < 0, 0, cv)
     sigma <- cv * (data_$mean)
     predLength <- vbgf_func(L1, L.inf, k, data_[, 1], a3)
     logLik <- sum(-log(sigma) - ((predLength - data_[, 2])^2)/(2*sigma^2))
     -logLik
+  }
+  
+  get_logistic_transform <- function(par, lo, hi){
+    par <- lo + (hi - lo)/(1+exp(-par))
+    return(par)
+  }
+  
+  inv_logistic <- function(par, lo, hi){
+    par <- 1-log(hi-lo)+log(par-lo)
+    return(par)
   }
 
   #function takes data, number of samples, start values, start age
@@ -50,12 +64,21 @@ sample_fit_vbgf <- function(length.data, start.L1, start.L2, start.k,
   length.df <- length.data[length.data$age > a3, ]
   length.df <- length.df[length.df$age < A, ]
   data_ <- length.df[, colnames(length.df) %in% c("length", "age", "mean")]
-  start.Linf<-start.L1+(start.L2-start.L1)/(1-exp(-start.k*(A-a3)))
+  start.Linf <- start.L1+(start.L2-start.L1)/(1-exp(-start.k*(A-a3)))
+  lo.Linf <- lo.L1+(lo.L2 - lo.L1)/(1-exp(-lo.k*(A-a3)))
+  hi.Linf <- hi.L1+(hi.L2 - hi.L1)/(1-exp(-hi.k*(A-a3)))
+  pars.mat <- matrix(cbind(c(start.L1,lo.L1,hi.L1),c(start.Linf,lo.Linf,hi.Linf),c(start.k,lo.k,hi.k),
+                  c(start.cv.young,lo.cv.young, hi.cv.young), c(start.cv.old, lo.cv.old, hi.cv.old)))
+  transformed <- rep(0,5)
+  for(i in 1:nrows(pars.mat)){
+    transformed[i] <- get_logistic_transform(pars.mat[i,])
+  }
+  
   #Fit using MLE
   mod <- mle2(get_vbgf_loglik,
-    start = list(logL1 = log(start.L1), logLinf = log(start.Linf),
-      logk = log(start.k), logcv.old = log(start.cv.old),
-      logcv.young=log(start.cv.young)))
+    start = list(logL1 = transformed[1], logLinf = transformed[2],
+      logk = transformed[3], logcv.old = transformed[4],
+      logcv.young=transformed[5]), )
   if(mod@details$convergence == 1 |
      grepl("Error", mod@coef[1], ignore.case = TRUE)){
     out <- list("L_at_Amin_Fem_GP_1" = 999, "L_at_Amax_Fem_GP_1" = 999,
@@ -63,12 +86,14 @@ sample_fit_vbgf <- function(length.data, start.L1, start.L2, start.k,
       "CV_old_Fem_GP_1" = 999)
   } else {
     #Put estimated coefficients in EM terms
-    expCoef <- exp(mod@coef)
-    cv.young <- expCoef[5]
-    cv.old <- expCoef[4]
-    L.inf<-expCoef[1]+(expCoef[2]-expCoef[1])/(1-exp(-expCoef[3]*(A-a3)))
-    out <- list("L_at_Amin_Fem_GP_1" = expCoef[1], "L_at_Amax_Fem_GP_1" = L.inf,
-      "VonBert_K_Fem_GP_1" = expCoef[3], "CV_young_Fem_GP_1" = cv.young,
+    logCoef <- mod@coef
+    L1 <- inv_logistic(logCoef[1], lo.L1, hi.L1)
+    L.inf <- inv_logistic(logCoef[2], lo.Linf, hi.Linf)
+    k <- inv_logistic(logCoef[3], lo.k, hi.k)
+    cv.young <- inv_logistic(logCoef[4], lo.cv.young, hi.cv.young)
+    cv.old <- inv_logistic(logCoef[5], lo.cv.old, hi.cv.old)
+    out <- list("L_at_Amin_Fem_GP_1" = L1, "L_at_Amax_Fem_GP_1" = L.inf,
+      "VonBert_K_Fem_GP_1" = k, "CV_young_Fem_GP_1" = cv.young,
       "CV_old_Fem_GP_1" = cv.old)
   }
   out
