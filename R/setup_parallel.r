4079766b26bba094f0f747299746db364b2fb86b
Sean Anderson <sean@seananderson.ca>
2015-05-07 10:31:50 -0400

    Tweak parallel setup for newer R CMD check

--- R/setup_parallel.r
+++ R/setup_parallel.r
@@ -1,13 +1,13 @@
 #' Setup parallel processing
 setup_parallel <- function() {
-  if(!require("foreach")) {
+  if(!requireNamespace("foreach", quietly = TRUE)) {
     stop("The foreach package is required for parallel scenario simulations.",
       call. = FALSE)
   }
   # to satisfy R CMD check:
   getDoParWorkers <- NULL
 
-  cores <- getDoParWorkers()
+  cores <- foreach::getDoParWorkers()
   if(cores == 1) {
     warning(paste("You have only registered one core. Reverting to non-parallel",
       "processing. You need to register multiple cores first. For example:",
