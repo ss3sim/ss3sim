233d008872c2e854ce3d7b41abc511eb21d3160e
kellijohnson <kellifayejohnson@gmail.com>
2015-12-19 08:26:44 -0800

    Add case_fishing, to write case file for fishing.

--- R/case-write.R
+++ R/case-write.R
@@ -169,6 +169,51 @@ case_tv <- function(species, parameter, perc_change, outfile,
   }
 }
 
+#' Write a case file for fishing data to the disk.
+#'
+#' Use \code{R} code to write arguments to the disk, which
+#' will later be used in a \code{ss3sim} simulation.
+#'
+#' @param years Vector of years for which \emph{F} values are specified,
+#' if there is more than one fleet or season the catches must be ordered by
+#' season:year:fishey (e.g., season1year1fishery1, season2year1fishery1,
+#' season1year2fishery1). The actual vector does not have to correspond to
+#' true years but must be the correct length (e.g., instead of
+#' \code{2000:2004} you can use \code{1:5}). Use this argument to create
+#' an index to old values. \code{years_alter} will use values in this vector.
+#' For example, with two seasons and one fishery that operates for 4 years
+#' you could use the following: \code{1:8}.
+#' @param years_alter Vector of years for the which \emph{F} values will be altered.
+#' If there is more than one fishery or season, use the mapping system
+#' created in \code{years} because actual year values cannot be recycled.
+#' For example, to change the second season of the second year in the
+#' example above, use: \code{4}.
+#' @param fvals Vector of \emph{F} values to be entered into \code{ss3.par} file,
+#' where \code{length(fvals) == length(years_alter)} must be true.
+#' @param case The case number you want to write to.
+#'   If \code{case = 1}, then the result will be \code{'F1'}.
+#' @param spp A vector of character values argument specifying the species.
+#' @export
+#' @examples
+#' case_fishing(1:100, 1:100, seq(0, 0.4, length.out = 100), 2, "cod")
+#' done <- file.remove("F2-cod.txt")
+case_fishing <- function(years = 1, years_alter = NULL, fvals = 2, case, spp) {
+
+  old <- options()$"deparse.cutoff"
+  options(deparse.cutoff = 500L)
+  on.exit(options(deparse.cutoff = old))
+
+  for (ind in seq_along(spp)){
+    filename <- paste0("F", case, "-", spp[ind], ".txt")
+    mapply(write, file = filename, MoreArgs = list(ncolumns = 2,
+      x = c("years;", case_deparse(years))))
+    mapply(write, file = filename, MoreArgs = list(ncolumns = 2, append = TRUE,
+      x = c("years_alter;", case_deparse(years_alter))))
+    mapply(write, file = filename, MoreArgs = list(ncolumns = 2, append = TRUE,
+      x = c("fvals;", case_deparse(fvals))))
+  }
+}
+
 #' Turn an argument describing an object into a character.
 #' @details Includes checks to make sure multiple lines will not be created.
 #' @param x The argument you would like to \code{deparse}.
