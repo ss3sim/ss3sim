1a30edccf06e067fdf64c498c6d5f1dcdf70addf
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2019-05-29 15:51:20 -0700

    fix: allow for blank line at top of file when finding version
    Use a while statement to find the first comment in a .ss file
    when trying to dynamically find the version.
    The forecast file had a blank line, which was causing the function to fail.

--- R/get_ss_version.R
+++ R/get_ss_version.R
@@ -14,14 +14,19 @@ get_ss_ver_dl <- function(dat_list){
   version
 }
 
-#' Get ths ss version (either 3.24 or 3.30) from an ss file (but not forecast.ss)
+#' Get ths ss version (either 3.24 or 3.30) from an ss file
 #'
 #' @description # Get the SS version from the top line in an SS file.
 #' as done in \code{\link[r4ss]{SS_readdat}}.
 #' @param file Input SS3 control file, either a starter, control, or data SS file.
 get_ss_ver_file <- function(file){
   # look for 3.24 or 3.30 at the top of the chosen control file
-  version <- scan(file, what=character(), nlines=1)
+  nl <- 0
+  version <- NULL
+  while (length(version) == 0) {
+    nl <- nl + 1
+    version <- scan(file, what=character(), nlines=nl, quiet = TRUE)
+  }
   version <- substring(version,3,6)
   if(version %in% c("3.24", "3.30")){ # perhaps too much output?
     #cat("assuming version", version, "based on first line of file\n")
