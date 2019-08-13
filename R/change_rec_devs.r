6069929ebf320df7cd813962ac0880444ec0fef0
k-doering <kathryn.doering@noaa.gov>
2019-08-13 10:57:24 -0700

    development: add tests for change_rec_devs, modify fun to make pass
    When adding tests for change_rec_devs, it was apparent that more checks were needed for invalid user input within the function.
    Partially addresses issue #255

--- R/change_rec_devs.r
+++ R/change_rec_devs.r
@@ -1,17 +1,17 @@
 #' Replace recruitment deviations
 #'
 #' This function replaces the recruitment deviations in the
-#' control file with those specified in \code{recdevs}. 
-#' The new control file is then written to the disk if 
-#' \code{ctl_file_out} is specified. 
+#' control file with those specified in \code{recdevs}.
+#' The new control file is then written to the disk if
+#' \code{ctl_file_out} is specified.
 #' It is imperative that the path provided in \code{ctl_file_in}
-#' be to a \code{ss_new} file so \code{change_rec_devs} can 
+#' be to a \code{ss_new} file so \code{change_rec_devs} can
 #' properly determine how many recruitment deviations to input
-#' into the control file. 
+#' into the control file.
 #'
 #' This function does not need to be specified in a case file if you
 #' are running an ss3sim simulation using \code{\link{run_ss3sim}}.
-#' 
+#'
 #' @param recdevs A vector of recruitment deviations to be entered into
 #' the SS control file. A single value will be repeated for every value in
 #' \code{years} or \code{length(years) == length(recdevs)} must be true.
@@ -23,40 +23,91 @@
 #'
 #' @examples
 #' d <- system.file(file.path("extdata", "models"), package = "ss3sim")
-#' change_rec_devs(recdevs = rlnorm(100), 
+#' change_rec_devs(recdevs = rlnorm(100),
 #'   ctl_file_in = file.path(d, "cod-om", "codOM.ctl"),
 #'   ctl_file_out = file.path(tempdir(), "control_recdevs.ss"))
 
-change_rec_devs <- function(recdevs, 
+change_rec_devs <- function(recdevs,
   ctl_file_in, ctl_file_out = "control_recruitment.ss") {
 
   ctl <- readLines(ctl_file_in)
+  default_err_msg <- paste0(" Please make sure you are using a control file ",
+                            "that was run through SS to get a control.ss_new ",
+                            "so that it has the default SS comments that ",
+                            "ss3sim expects.")
   devs <- grep("#\\s*[0-9]+[RFE]", ctl, value = TRUE)
   years <- unlist(strsplit(
-    gsub("^_", "", 
+    gsub("^_", "",
     gsub("^#\\s*|\\s|R|F|E", "_", devs)), "_+"))
   n.years <- length(years)
+  if(length(devs) == 0 | n.years == 0) {
+    stop("The number of recdevs and their associated years could not be ",
+         "determined from the control file.", default_err_msg)
+  }
+  if(length(recdevs) < n.years & length(recdevs)>1) {
+    stop("The length of recdevs was greater than 1, but smaller than the number ",
+         "of years required. Please change to have either length of 1 or at ",
+         "least ", n.years, " values")
+   } else if(length(recdevs) == 1) {
+     recdevs <- rep(recdevs, length.out = n.years)
+   }
   recdevs <- recdevs[seq_along(years)]
   newdata <- data.frame(
     "Yr" = years,
     "recdev" = recdevs)
-    
-  locations <- grep("do_recdev", ctl)
-  ctl[locations] <- gsub("^[0-4]\\s*", "1 ", trimws(ctl[locations]))
-  ctl[locations + 4] <- gsub("^[0-1]\\s*", "1 ", trimws(ctl[locations + 4]))
-  locations <- grep("read_recdevs|Fishing Mortality info", ctl)
-  if (length(locations) > 2) stop("Fishing Mortality info was found more", 
-    " than once in the control file")
-  if (length(locations) == 1) {
-    locations[2] <- grep("F ballpark$", ctl)
+  if(anyNA(newdata)) {
+    stop("change_rec_devs is attempting to add in NA values for recruitment ",
+         "deviations values or associated years in the control file. Please ",
+         "contact the ss3sim developers for help with this issue.")
   }
-  ctl[locations[1]] <- gsub("^[0-9]+\\s*", paste0(n.years, " "), 
+  locations <- grep("do_recdev", ctl) # find start of recruitmen devs section
+  #turn on recruitment devs and advanced options
+  ctl[locations] <- gsub("^[0-4]\\s*", "1 ", trimws(ctl[locations]))
+  # TODO: add ability to turn on advanced options with this script if they are
+  # off. For now, provide an informative error.
+  read_adv_option <- as.integer(strsplit(trimws(ctl[locations + 4]), "\\s+")
+                                [[1]][1])
+  if(read_adv_option == 0) {
+    stop("Currently ss3sim can only add recruitment deviations to control ",
+      "files that have advanced recruitment options turned on. In the OM ",
+      "control file, please change value on line with comment '# (0/1) to read",
+      " 13 advanced options' from 0 to 1 and add values for other advanced ",
+      "options into the OM control file.")
+    #following line turns on advanced recdevs options:
+    # ctl[locations + 4] <- gsub("^[0-1]\\s*", "1 ", trimws(ctl[locations + 4]))
+    # Add other code to add in the other lines needed instead of stopping.
+  } else if(read_adv_option != 1){
+    stop("The value in the OM control file to read advanced options is ",
+         read_adv_option, "which is not a valid input.")
+  }
+  # Find location to insert recdevs.
+  locations <- grep("read_recdevs", ctl)
+  if(length(locations) == 0) {
+    stop("ss3sim could not find the line with comment '#_read_recdevs' in the ",
+         "OM control file.", default_err_msg)
+  }
+  locations <- c(locations, grep("Fishing Mortality info", ctl))
+  if (length(locations) > 2) {
+    stop("Fishing Mortality info was found more",
+    " than once in the control file")
+  }
+  if (length(locations) == 1) {
+    tmp_loc <- grep("F ballpark$", ctl)
+    if(length(tmp_loc) == 1){
+      locations[2] <- grep("F ballpark$", ctl)
+    } else {
+      stop("Could not find spot where Fishing Mortality info starts.",
+           default_err_msg)
+    }
+  }
+  # insert recdevs.
+  ctl[locations[1]] <- gsub("^[0-9]+\\s*", paste0(n.years, " "),
     trimws(ctl[locations[1]]))
   ctl <- ctl[-((locations[1] + 1):(locations[2] - 1))]
-  ctl <- append(ctl, 
+  ctl <- append(ctl,
     values = apply(newdata, 1, paste, collapse = " "),
     after = locations[1])
-  
+
   # Write new control file
   if (!is.null(ctl_file_out)) {
     writeLines(ctl, con = ctl_file_out)
--- tests/testthat/test-recdevs.R
+++ tests/testthat/test-recdevs.R
@@ -1,24 +1,118 @@
 context("Test recdev functions")
-#user recdevs, change_recdevs, get_recdevs()
+#user recdevs, change_recdevs.
+#get_recdevs() is pretty simple, so does not need a test for now.
+
+temp_path <- file.path(tempdir(), "test-recdevs")
+dir.create(temp_path, showWarnings = FALSE)
+wd <- getwd()
+setwd(temp_path)
+on.exit(setwd(wd), add = TRUE)
+on.exit(unlink(temp_path, recursive = TRUE), add = TRUE)
+
+d <- system.file("extdata", package = "ss3sim")
+om <- file.path(d, "models", "cod-om")
+em <- file.path(d, "models", "cod-em")
+case_folder <- file.path(d, "eg-cases")
+
+# copy control file to temp_path.
+file.copy(file.path(om, "codOM.ctl"), "codOM.ctl", overwrite = TRUE)
+
 test_that("run_ss3sim fails if ncol(user_recdevs) is too small for iterations", {
-
-  temp_path <- file.path(tempdir(), "ss3sim-test")
-  dir.create(temp_path, showWarnings = FALSE)
-  wd <- getwd()
-  setwd(temp_path)
-
-  d <- system.file("extdata", package = "ss3sim")
-  om <- paste0(d, "/models/cod-om")
-  em <- paste0(d, "/models/cod-em")
-  case_folder <- paste0(d, "/eg-cases")
-
   urd <- matrix(data = 0, ncol = 1, nrow = 100)
-
   # one too many iterations for user_recdevs:
   expect_error(run_ss3sim(iterations = 2,
     scenarios = "D0-E0-F0-R0-M0-cod", case_folder = case_folder,
-    om_dir = om, em_dir = em, user_recdevs = urd))
+    om_dir = om, em_dir = em, user_recdevs = urd),
+    "The number of columns in user_recdevs is less than the specified number of iterations")
 })
-# TODO: add change_recdevs() tests
+test_that("change_rec_devs() works with vector of recdevs", {
+  set.seed(123)
+  recdevs <- rnorm(2000, 0, 1) #don't worry about bias correction here.
+  out_ctl <- change_rec_devs(recdevs, ctl_file_in = "codOM.ctl",
+                             ctl_file_out = NULL)
+  n_recdevs_line <- grep("#_read_recdevs", out_ctl, fixed = TRUE)
+  n_recdevs <- as.numeric(strsplit(trimws(out_ctl[n_recdevs_line]), "\\s+")[[1]][1])
+  f_recdev_line <-n_recdevs_line+1
+  l_recdev_line <- grep("#Fishing Mortality info", out_ctl, fixed = TRUE)-1
+  # check that the number of years corresponds with number of lines inserted.
+  expect_equal(n_recdevs, l_recdev_line-f_recdev_line+1)
+})
 
-# TODO, but lower priority: add get_recdevs() tests
+test_that("change_rec_devs works with single recdev value", {
+  out_ctl <- change_rec_devs(0.01, "codOM.ctl", NULL)
+  n_recdevs_line <- grep("#_read_recdevs", out_ctl, fixed = TRUE)
+  n_recdevs <- as.numeric(strsplit(trimws(out_ctl[n_recdevs_line]), "\\s+")[[1]][1])
+  f_recdev_line <-n_recdevs_line+1
+  l_recdev_line <- grep("#Fishing Mortality info", out_ctl, fixed = TRUE)-1
+  # check that the number of years corresponds with number of lines inserted.
+  expect_equal(n_recdevs, l_recdev_line-f_recdev_line+1)
+})
+
+test_that("change_rec_devs gives error when provided too few recdevs", {
+  set.seed(123)
+  recdevs <- rnorm(2, 0, 1) # too short
+  expect_error(change_rec_devs(recdevs, ctl_file_in = "codOM.ctl",
+                  ctl_file_out = NULL),
+               "The length of recdevs was greater than 1, but smaller than ")
+})
+
+test_that("change_rec_devs gives error when cannot find info on number and yrs for recdevs", {
+  set.seed(123)
+  recdevs <- rnorm(200, 0, 1)
+  ctl <- readLines("codOM.ctl")
+  #get rid of recrutiment dev lines that ss3sim uses to determine recdevs number
+  tmp_start <- grep("# all recruitment deviations", ctl , fixed = TRUE)
+  tmp_end <- grep("implementation error by year in forecast", ctl, fixed = TRUE)
+  writeLines(ctl[-(tmp_start:tmp_end)], "codOM_mod.ctl")
+  expect_error(change_rec_devs(recdevs, "codOM_mod.ctl", NULL),
+               paste0("The number of recdevs and their associated years could",
+                       " not be determined from the control file"))
+})
+
+test_that("change_rec_devs give error when advanced recdevs options are off",{
+  #Note: would expect this test to fail if change_rec_devs is able to deal with
+  # this scenario in the future.
+  set.seed(123)
+  ctl <- readLines("codOM.ctl")
+  s_adv <- grep("to read 13 advanced options", ctl)
+  e_adv <- grep("end of advanced SR options", ctl)
+  ctl[s_adv] <- "0"
+  ctl <- ctl[-((s_adv+1):e_adv)]
+  writeLines(ctl, "codOM_no_adv.ctl")
+  expect_error(change_rec_devs(rnorm(200), "codOM_no_adv.ctl", NULL),
+              paste0("Currently ss3sim can only add recruitment deviations to ",
+                     "control files that have advanced recruitment options ",
+                     "turned on"))
+})
+
+test_that("change_rec_devs looks for spot to put recdevs as expected",{
+  set.seed(123)
+  recdevs <- rnorm(200)
+  ctl <- readLines("codOM.ctl")
+  # missing starting spot.
+  recdev_line <- grep("_read_recdevs", ctl)
+  mod_ctl <- ctl
+  mod_ctl[recdev_line] <- "0" # removes comment.
+  writeLines(mod_ctl, "codOM_no_recdevs_cmt.ctl")
+  expect_error(change_rec_devs(recdevs, "codOM_no_recdevs_cmt.ctl", NULL),
+               paste0("ss3sim could not find the line with comment ",
+               "'#_read_recdevs' in the OM control file"), fixed = TRUE)
+  # missing both F comments
+  mod_ctl <- ctl
+  F_line <- grep("Fishing Mortality info", mod_ctl)
+  F_ball_line <- grep("F ballpark$", mod_ctl)
+  mod_ctl <- mod_ctl[-c(F_line, F_ball_line)]
+  writeLines(mod_ctl, "codOM_missing_2_F_cmt.ctl")
+  expect_error(change_rec_devs(recdevs, "codOM_missing_2_F_cmt.ctl", NULL),
+               "Could not find spot where Fishing Mortality info starts")
+  #only missing 1 F comment (can run)
+  mod_ctl <- ctl
+  mod_ctl <- mod_ctl[-F_line]
+  writeLines(mod_ctl, "CodOM_missing_1_F_cmt.ctl")
+  out_ctl <- change_rec_devs(recdevs, "CodOM_missing_1_F_cmt.ctl", NULL)
+  n_recdevs_line <- grep("#_read_recdevs", out_ctl, fixed = TRUE)
+  n_recdevs <- as.numeric(strsplit(trimws(out_ctl[n_recdevs_line]), "\\s+")[[1]][1])
+  f_recdev_line <-n_recdevs_line+1
+  l_recdev_line <- grep("F ballpark$", out_ctl)-1
+  expect_equal(n_recdevs, l_recdev_line-f_recdev_line+1)
+})
