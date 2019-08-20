7f40f0ea12f94b8524462a6201d94f65eee30a95
Kelli Johnson <kelli.johnson@noaa.gov>
2019-08-20 09:08:16 -0700

    documentation: reword fleets, ESS, and cpar input argument documentation

--- man-roxygen/lcomp-agecomp-index.R
+++ man-roxygen/lcomp-agecomp-index.R
@@ -1,7 +1,6 @@
-#' @param fleets *A numeric vector giving the fleets to be used. This order also
-#' pertains to other arguments. A missing value excludes that fleet from
-#' \code{outfile} (i.e. it turns it off so no samples are written). If none of
-#' the fleet collected samples, keep the value to \code{fleets=NULL}.
+#' @param fleets *A vector of integers specifying which fleets to include.
+#' The order of the fleets pertains to the input order of other arguments.
+#' An entry of \code{fleets=NULL} leads to zero samples for any fleet. 
 #' @param years *A list the same length as \code{fleets} giving the years as
 #' numeric vectors. If no fleet collected samples, keep the value to
 #' \code{years=NULL}.
--- man-roxygen/lcomp-agecomp.R
+++ man-roxygen/lcomp-agecomp.R
@@ -1,11 +1,18 @@
-#' @param ESS An effective sample size to write to file (but not use in
-#' sampling). The default of NULL means to use the true (internally
-#' calculated) ESS, which is \code{Nsamp} for the multinomial case or given
-#' by the formula under \code{cpar} for the Dirichlet case. Must match the
-#' structure of the \code{years} arguments.
-#' @param cpar *A numeric value or vector the same length as
+#' @param ESS The final effective sample size (ESS) associated with the
+#' simulated data. The ESS is not used to generate the simulated data
+#' but can be used as an input sample size in subsequent models that estimate
+#' population parameters or status.
+#' The default, \code{NULL}, leads to the true (internally calculated)
+#' #' ESS being used, which is \code{Nsamp} for the multinomial case or given
+#' by the formula under \code{cpar} for the Dirichlet case.
+#' At least one value must be provided for each fleet or a vector of
+#' year-specific values can be used for any given fleet.
+#' The argument accepts a list with entries,
+#' either a single integer or a vector of integers, for each fleet.
+#' @param cpar A numeric value or vector the same length as
 #' \code{fleets} controlling the variance of the Dirichlet
-#' distribution used for sampling. A value of \code{1} indicates the
+#' distribution used for sampling. A value of \code{1} leads to the
 #' same standard deviation as a multinomial of the given \code{Nsamp},
 #' \code{2} indicates twice, etc. Values greater than one indicate
-#' overdispersion, and less underdispersion.
+#' overdispersion, and less underdispersion. \code{NULL} or \code{NA}
+#' for a given fleet will lead to no dispersion.
