c837b64be6c63d571dd9a83ce59843ce0df1e927
Kelli Johnson <kelli.johnson@noaa.gov>
2019-08-22 07:00:26 -0700

    document: remove example from change_rec_devs_par
    Remove the example because it used the par file that is no longer available
    in the cod model folder for the OM because ss3sim changed to using the
    control file. This function will still be available for those that
    want to manipulate a par file for the recdevs.

--- R/change_rec_devs_par.r
+++ R/change_rec_devs_par.r
@@ -15,15 +15,6 @@
 #'   \code{\link{run_ss3sim}}.
 #' @export
 #'
-#' @examples
-#' # Create a temporary folder for the output:
-#' temp_path <- file.path(tempdir(), "ss3sim-recdev-example")
-#' dir.create(temp_path, showWarnings = FALSE)
-#'
-#' par_file <- system.file("extdata", "models", "cod-om", "ss.par",
-#'   package = "ss3sim")
-#' change_rec_devs(recdevs_new = rlnorm(100), par_file_in = par_file,
-#'   par_file_out = paste0(temp_path, "/test.par"))
 
 change_rec_devs_par <- function(recdevs_new, par_file_in = "ss.par",
   par_file_out="ss.par"){
