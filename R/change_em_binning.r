7a1393d8d82e12da37ee8ce2116ba892baafcd77
Kelli Johnson <kelli.johnson@noaa.gov>
2019-08-22 06:50:13 -0700

    document: fix error wrt years in examples in change_em_binning
    also simplified the input to not use a 3.24 data file from example-om

--- R/change_em_binning.r
+++ R/change_em_binning.r
@@ -31,21 +31,17 @@
 #' @author Kotaro Ono (length-composition rebinning), Sean Anderson
 #'   (conditional age-at-length rebinning)
 #' @examples
-#' d <- system.file("extdata", package = "ss3sim")
-#' f_in <- paste0(d, "/example-om/data.ss_new")
-#' dat_list <- r4ss::SS_readdat(file = f_in, version = NULL, verbose = FALSE)
-#' l <- change_em_binning(dat_list, outfile = NULL, lbin_method = 1,
-#'   bin_vector = seq(8, 30, by = 1))
-#' print(l$lbin_vector)
-#' print(head(l$lencomp))
-#'
 #' # An small example with conditional age-at-length re-binning:
 #' f <- system.file("extdata", "models", "cod-om", "codOM.dat", package = "ss3sim")
 #' d <- r4ss::SS_readdat(f, version = NULL, verbose = FALSE)
-#'
+#' l <- change_em_binning(d, outfile = NULL, lbin_method = 1,
+#'   bin_vector = seq(20, 152, by = 4))
+#' print(l$lbin_vector)
+#' print(head(l$lencomp))
 #' # Add catch at length data (and simplify the bin structure for this example)
-#' olddat <- change_data(d, outfile = NULL, 
-#'   types = c("len", "age", "cal"), fleets = 1, years = seq(2000, 2002),
+#' olddat <- change_data(d, outfile = NULL,
+#'   types = c("len", "age", "cal"), fleets = 1,
+#'   years = seq(d$styr, d$styr + 4),
 #'   age_bins = 1:3, len_bins = 4:8)
 #' olddat$agecomp
 #' newdat <- change_em_binning(olddat, outfile = NULL, bin_vector = c(4, 6, 8),
@@ -53,8 +49,9 @@
 #' newdat$agecomp
 #'
 #' # A larger conditional age-at-length re-rebinning example:
-#' olddat <- change_data(d, outfile = NULL, 
-#'  types = c("len", "age", "cal"), fleets = 1, years = seq(2000, 2005),
+#' olddat <- change_data(d, outfile = NULL,
+#'  types = c("len", "age", "cal"), fleets = 1,
+#'  years = seq(d$styr, d$styr + 4),
 #'  age_bins = seq(1, 5), len_bins = round(seq(20, 30, length.out = 13), 1))
 #'
 #' olddat$lbin_vector
