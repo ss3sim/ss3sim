0ef373a92074448a97bd65d3970832072aa8dde1
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2019-09-17 15:11:17 -0700

    test: remove extra tests

--- tests-extra/devel.R
+++
@@ -1,5 +0,0 @@
-install.packages(c("foreach", "plyr", "gtools", "lubridate", "ggplot2", "bbmle",
-  "dplyr", "magrittr"), type = "source")
-install.packages(c("reshape2", "doParallel", "knitr"), type = "source")
-install.packages(c("coda", "corpcor", "gplots", "maps", "pso", "RCurl"), type = "source")
-install.packages("../r4ss/", type = "source", repos = NULL)
--- tests-extra/run_tests.R
+++
@@ -1,9 +0,0 @@
-## This is a central file from which to source the different testing
-## scripts.
-
-## Make sure wd is set to this testing folder first.
-devtools::load_all('..')
-
-source("test_tail_compression.R")
-source("test_lcomp_constant.R")
-
