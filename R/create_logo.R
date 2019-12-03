c962f615548c4c6f4c74fcb096910cf766e35720
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2019-12-02 20:19:01 -0800

    fix: add bindings for functions in create_logo
    png, data, quantile, polygon, and lines were not tied to a package
    the example wasn't recognized b/c create_logo isn't exported

--- R/create_logo.R
+++ R/create_logo.R
@@ -5,19 +5,19 @@
 #' 
 #' @template outfile
 #' @examples
-#' create_logo()
+#' ss3sim:::create_logo()
 #' dev.off()
-#' @return A \code{.png} file or a graphics device with the 
+#' @return A \code{png} file or a graphics device with the 
 #' logo used for the \code{ss3sim} project.
 #' @author Kelli Faye Johnson
 #' 
 create_logo <- function(outfile = NULL) {
   if (!is.null(outfile)) {
-    png(outfile,
+    grDevices::png(outfile,
       width = 4, height = 4, units = "in", res = 600)
     on.exit(dev.off())
   }
-  data(ts_dat, package = "ss3sim")
+  utils::data(ts_dat, package = "ss3sim")
   # cols <- RColorBrewer::brewer.pal(8, "Blues")
   cols <- c("#F7FBFF", "#DEEBF7", "#C6DBEF", "#9ECAE1",
     "#6BAED6", "#4292C6", "#2171B5", "#084594")
@@ -28,18 +28,18 @@ create_logo <- function(outfile = NULL) {
   ts_dat$Recruit_0 <- (ts_dat$Recruit_0_em - ts_dat$Recruit_0_om) / 
     ts_dat$Recruit_0_om
   quant_dat <- data.frame(do.call("rbind", 
-    tapply(ts_dat$SpawnBio, ts_dat$year, quantile, 
+    tapply(ts_dat$SpawnBio, ts_dat$year, stats::quantile, 
     probs = c(0.05,0.25, 0.50, 0.75, 0.95))))
   colnames(quant_dat) <- c("q05", "q25", "q50", "q75", "q95")
   quant_dat$year <- row.names(quant_dat)
 
   plot(1, 1, 
-    xlim = ceiling(quantile(ts_dat$year, probs = c(0.03, 0.32))), 
+    xlim = ceiling(stats::quantile(ts_dat$year, probs = c(0.03, 0.32))), 
     ylim = c(-0.15, 0.2),
     type = "n", axes = FALSE, ann = FALSE, xaxs = "i")
-  polygon(c(quant_dat$year, rev(quant_dat$year)), c(quant_dat$q05, rev(quant_dat$q95)), 
+  graphics::polygon(c(quant_dat$year, rev(quant_dat$year)), c(quant_dat$q05, rev(quant_dat$q95)), 
     col = cols[4], border = NA)
-  polygon(c(quant_dat$year, rev(quant_dat$year)), c(quant_dat$q25, rev(quant_dat$q75)), 
+  graphics::polygon(c(quant_dat$year, rev(quant_dat$year)), c(quant_dat$q25, rev(quant_dat$q75)), 
     col = cols[6], border = NA)
-  lines(quant_dat$year, quant_dat$q50, col = cols[8], lwd = 3)
+  graphics::lines(quant_dat$year, quant_dat$q50, col = cols[8], lwd = 3)
 }
