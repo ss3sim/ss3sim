4d443d587cb9a837a58f90fff6fa6a86a805f3a0
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2020-02-24 07:43:58 -0800

    fix: Increase checks get_success when covar file doesn't exist

--- R/get_success.R
+++ R/get_success.R
@@ -21,7 +21,8 @@ get_success <- function(dir) {
   }
   par_file_in <- dir(dir, pattern = ".+par$")
   cor_file_in <- gsub("par", "cor", par_file_in)
-  if (file.exists(file.path(dir, cor_file_in))) {
+  if (file.exists(file.path(dir, cor_file_in)) &&
+      file.exists(file.path(dir, "covar.sso"))) {
     outvec["hess"] <- 1
   }
   return(outvec)
