433f44dfb6025acfe3e7bbe427502d4797a960e5
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-05-07 09:02:36 -0700

    feature: calculate RE from wide or long data format
    * allow for calculate_re to work with the new long data by
    sourcing convert_to_wide internally
    * don't expect users to change from long to wide
    * subset data for a single EM where the default EM is the name used
    by ss3sim of em.

--- R/calculate_re.R
+++ R/calculate_re.R
@@ -7,12 +7,17 @@
 #' @param dat An input data frame. Should be either a scalar or time series
 #'   data frame as returned from \code{\link{get_results_all}} or a related
 #'   get results function. Specifically, the data frame needs to have columns
-#'   with \code{_em} and \code{_om} as names.
+#'   with \code{_em} and \code{_om} as names. If the data is provided in long
+#'   rather than wide format, then \code{\link{convert_to_wide}} will be used
+#'   internally before calculating RE and a wide data frame will be returned.
 #' @param add Logical: should the relative error columns be added to \code{dat}
 #'   or should the original EM and OM columns be dropped? If \code{FALSE} then
 #'   the returned data frame will have only the identifying columns and the new
 #'   relative error columns. You could then merge selected columns back into
 #'   \code{dat} if you wished. The default is to return all columns.  
+#' @param EM A character value specifying the name of the EM to calculate the
+#'   RE of when the results are provided in long format and there is the potential
+#'   for multiple EMs. See the column \code{model_run} for options.
 #' @author Sean Anderson and Cole Monnahan
 #' @seealso \code{\link{get_results_all}}, \code{link{get_results_scenario}}
 #' @return The default is to return a data frame structured the same as the 
@@ -30,8 +35,13 @@
 #' head(calculate_re(scalar_dat, add = FALSE))
 #' rm("ts_dat", "scalar_dat")
 #'
-calculate_re <- function(dat, add = TRUE) {
+calculate_re <- function(dat, add = TRUE, EM = "em") {
 
+  # Check if wide or long data
+  if ("model_run" %in% colnames(dat)) {
+    stopifnot(length(EM) == 1)
+    dat <- convert_to_wide(dat[dat$model_run == EM, ])
+  }
   both <- intersect(
     gsub("_em", "", grep("_em", names(dat), value = TRUE)),
     gsub("_om", "", grep("_om", names(dat), value = TRUE)))
