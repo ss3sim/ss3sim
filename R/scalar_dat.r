2cef4f48073aa2be18ab5cd135295bed0dc6334e
kellijohnson-NOAA <kelli.johnson@nooa.gov>
2020-05-13 11:56:10 -0700

    documentation: better document data sets

--- R/scalar_dat.r
+++ R/scalar_dat.r
@@ -1,6 +1,19 @@
-#' Example scalar data from the ss3sim vignette
+#' Example scalar data from the Introduction vignette
+#'
+#' An R object read in using \code{read.csv("ss3sim_scalar.csv")} after
+#' processing the results from the Introduction vignette
+#' using \code{get_results_all()}. The data set is available
+#' so users do not have to wait for the scenarios to run.
 #'
 #' @name scalar_dat
 #' @docType data
 #' @keywords data
+#' @examples
+#' data("scalar_dat", package = "ss3sim")
+#'
+#' \dontshow{
+#' testthat::expect_true("max_grad" %in% colnames(scalar_dat),
+#'   label = "the column 'max_grad' of the scalar data set")
+#' }
+#'
 NULL
--- R/ts_dat.r
+++ R/ts_dat.r
@@ -1,6 +1,19 @@
-#' Example time series data from the ss3sim vignette
+#' Example time-series data from the Introduction vignette
+#'
+#' An R object read in using \code{read.csv("ss3sim_ts.csv")} after
+#' processing the results from the Introduction vignette
+#' using \code{get_results_all()}. The data set is available
+#' so users do not have to wait for the scenarios to run.
 #'
 #' @name ts_dat
 #' @docType data
 #' @keywords data
+#' @examples
+#' data("ts_dat", package = "ss3sim")
+#'
+#' \dontshow{
+#' testthat::expect_true("year" %in% colnames(ts_dat),
+#'   label = "the column 'year' of the time-series data set")
+#' }
+#'
 NULL
