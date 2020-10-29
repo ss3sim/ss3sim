d7c721fa6d7072db2e052042f730780395a9d9c0
kathryn.doering <kathryn.doering@noaa.gov>
2020-10-29 14:55:55 -0700

    fix: allow multiple seasons in catch

--- R/change_catch.R
+++ R/change_catch.R
@@ -7,9 +7,20 @@
 #'
 change_catch <- function(dat_list, f_params) {
   # Note: would need to modify if did not want catch every year, potentially.
-  dat_list[["catch"]] <- expand.grid(
-    year = c(-999, dat_list$styr:dat_list$endyr),
-    seas = 1, fleet = f_params$fleets, catch = 1, catch_se = 0.01)
+  #if no seasons provided, assume seasons for each fleet.
+  if(is.null(f_params[["seas"]])) {
+    nseas <- dat_list$nseas
+    f_params$seas <- lapply(f_params$fleets, function(x, n) seq_len(n),
+                            n = nseas)
+  }
+  dat_list[["catch"]] <- NULL
+  for(f in seq_len(f_params$fleets)) {
+    tmp_catch <- expand.grid(
+      year = c(-999, dat_list$styr:dat_list$endyr),
+      seas = f_params$seas[[f]], fleet = f_params$fleets[f], catch = 1,
+      catch_se = 0.01)
+    dat_list[["catch"]] <- rbind(dat_list[["catch"]], tmp_catch)
+  }
   dat_list[["catch"]][dat_list[["catch"]][, "year"] == -999, "catch"] <- 0
   dat_list
 }
