b716d0d2ac4488ebb1155845e57adc48118d0371
kathryn.doering <kathryn.doering@noaa.gov>
2021-01-22 15:16:19 -0800

    tests: rm scripts that break up tests
    this was necessary for travis, but isn't for github acations

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,4 @@
 library("testthat")
 library("ss3sim")
-# broke up tests to allow tests to run longer than 10 min in travis
-# see https://github.com/travis-ci/travis-ci/issues/3849#issuecomment-345686242
-# note that if tests in test that script do not start with "test.", they will not
-# run
-test_check("ss3sim", filter = "^[a-q]")
+
+test_check("ss3sim")
