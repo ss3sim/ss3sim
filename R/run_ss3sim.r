34d0b0ae895c1a5183b95543059f5474042bce31
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2022-06-09 01:15:57 -0700

    docs(run_ss3sim): Includes better links, more details

--- R/run_ss3sim.r
+++ R/run_ss3sim.r
@@ -1,27 +1,47 @@
-#' Master function to run simulations using Stock Synthesis
+#' Master function to run a set of simulations using \pkg{ss3sim}
 #'
-#' This is the main high-level wrapper function for running \pkg{ss3sim}
-#' simulations. First, this function separates the data frame of simulation
-#' settings by row for each scenario into a list format. These lists are then
-#' passed to [ss3sim_base()] to run each simulation. Alternatively, you
-#' can run [ss3sim_base()] directly using your own lists.
+#' This is the main high-level wrapper function used to run
+#' a set of \pkg{ss3sim} simulations.
+#' The data frame passed to `simdf` is parsed into a list and used to control
+#' [ss3sim_base()]. Alternatively, you can call [ss3sim_base()] directly
+#' with your own lists.
 #'
-#' @param iterations Which iterations to run. A numeric vector. For example
-#'   `1:100`. The same number of iterations will be run for each scenario.
-#'   If any iterations already exist, then ss3sim will skip over them.
+#' @param iterations A numeric vector specifying which iterations are desired.
+#'   For example `1:100`.
+#'   The same number of iterations will be ran for each scenario.
+#'   If any iterations already have a folder from a previous run, they will be
+#'   skipped even if they do not contain viable results.
 #' @param simdf A data frame of instructions with one row per scenario.
 #'   See [setup_scenarios_defaults()] for default values that will be
-#'   passed to ss3sim for a generic simulation to get you started. These
-#'   values will be used if `simdf` is left at its default value of `NULL`.
+#'   used for a generic simulation to get you started.
+#'   These default values will only work with the stored cod model because
+#'   some of the columns in `simdf` need to have values that
+#'   match the fleet structure of the operating model. If you are not using the
+#'   default cod model, please remember to add `om_dir` and `em_dir` columns
+#'   to `simdf` with file paths to the locations of your operating model and
+#'   estimation method. Essentially, `simdf` is a way to pass scenario-specific
+#'   information to the arguments of [ss3sim_base()], whereas the `...` method
+#'   will only work for things like `seed` that are universal to all scenarios
+#'   in a simulation.
 #' @param parallel A logical argument that controls whether the scenarios are
 #'   run in parallel. You will need to register multiple cores first with a
 #'   package such as \pkg{doParallel} and have the \pkg{foreach} package
-#'   installed. See the example below.
-#' @param parallel_iterations Logical. By default `parallel = TRUE` will
-#'   run scenarios in parallel. If you set `parallel = TRUE` and
-#'   `parallel_iterations = TRUE` then the iterations will be run in
-#'   parallel. This would be useful if you were only running one scenario
-#'   but you wanted to run it faster.
+#'   installed. For example, the following code will register two cores
+#'   and must be called before running `run_ss3sim()`:
+#'
+#'   ```
+#'   library(doParallel)
+#'   cl <- makeCluster(2)
+#'   registerDoParallel(cl)
+#'   ```
+#' @param parallel_iterations A logical argument specifying if you wish to
+#'   run iterations in parallel. If you set `parallel = TRUE` and
+#'   `parallel_iterations = TRUE` then iterations for a given scenario will
+#'   be sent to multiple processors. All iterations for a given scenario must
+#'   finish before the next scenario is started.
+#'   This will be useful if you want to run one scenario fast or if you want
+#'   to be able to look at the results for each scenario as they finish in
+#'   another instance of \R. The argument will be ignored if `parallel = FALSE`.
 #' @param ... Anything else to pass to [ss3sim_base()]. This could
 #'   include `bias_adjust`. Also, you can pass
 #'   additional options to the executable through the argument
@@ -29,20 +49,29 @@
 
 #' @author Sean C. Anderson
 #'
-#' @details The operating model folder should contain: `forecast.ss`,
-#' `yourmodel.ctl`, `yourmodel.dat`, `ss.par`, and
-#' `starter.ss`. The files should be the versions that are returned from an
-#' Stock Synthesis run as `.ss_new` files. This is important because it creates
-#' consistent formatting which many of the functions in this package depend on.
-#' Rename the `.ss_new` files as listed above (and in all lowercase). The
-#' estimation model folder should contain all the same files listed above except
-#' the `ss.par` and `yourmodel.dat` files, which are unnecessary but
-#' can be included if desired. See the vignette for details on modifying an
-#' existing Stock Synthesis model to run with \pkg{ss3sim}. Alternatively, you might
-#' consider modifying one of the built-in model configurations.
+#' @details The operating model folder, which is passed as a file path
+#' using `simdf[["om_dir"]]`, should contain the following files:
+#' * `forecast.ss`,
+#' * `yourmodel.ctl`,
+#' * `yourmodel.dat`,
+#' * `ss.par`, and
+#' * `starter.ss`.
+#' The files should be the formatted versions that are returned from
+#' Stock Synthesis after the model is optimized, i.e., `.ss_new` files.
+#' It is important to use these formatted files because many functions in
+#' \pkg{ss3sim} and \pkg{r4ss} depend on the location of keywords present
+#' in the comments and other standardized formatting. Once you have these
+#' files from a successfully optimized model, rename the `.ss_new` files
+#' to match the names listed above, though you can change `yourmodel` to
+#' whatever name is listed for the control and data files in `starter.ss`.
+#' The estimation model folder should also contain these files, except
+#' `ss.par` and `yourmodel.dat` files, which are unnecessary.
+#' See the vignette titled modifying-models for details on modifying an
+#' existing Stock Synthesis model to run with \pkg{ss3sim}. Alternatively,
+#' consider modifying the built-in model configuration based on north sea cod.
 #'
-#' Note that due to the way that Stock Synthesis is being used as an OM, you may see
-#' the following ADMB error may appear in the console:
+#' Note that due to the way that Stock Synthesis is being used as an OM,
+#' you may see the following error from ADMB in the console:
 #' Error -- base = 0 in function prevariable& pow(const prevariable& v1, CGNU_DOUBLE u)
 #' However, this is not a problem because ADMB is not used to optimize the OM,
 #' and thus, the error can safely be ignored.
@@ -50,7 +79,7 @@
 #' @importFrom foreach %dopar%
 #'
 #' @return
-#' The output will appear in your current \R working directory
+#' The output will appear in your current \R working directory.
 #' Folders will be named based on the `"scenario"` column
 #' of `simdf` or based on the date-time stamp
 #' (i.e., mmddhhmmss) generated automatically at the start of the simulation.
@@ -61,14 +90,10 @@
 #' * `0101120000/2/om`
 #' * ...
 #'
-# An illustration of the input and output file structure
-# of an \pkg{ss3sim} simulation:
-# \figure{filestructure.png}{An illustration of the input and output file
-# structure for an ss3sim simulation.}
-#'
 #' @seealso
-#' [ss3sim_base()] can be called directly by passing lists of arguments,
-#' rather than using the data frame approach with `run_ss3sim`.
+#' [ss3sim_base()] can be called directly by passing lists to each
+#' individual argument rather than using the data-frame approach
+#' of `run_ss3sim(simdf = )`.
 #' The lists correspond to each function called by [ss3sim_base()].
 #' Each element is itself a list of arguments for the given function.
 #' Either way allows users to pass arguments to each of the
