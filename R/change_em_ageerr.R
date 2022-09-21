fe0e73c4cbc46ea4c4c0110e27577dc79ed891b0
chamberlindw <derek.w.chamberlin@gmail.com>
2022-09-21 13:40:58 -0400

    Added change_em_ageerr.R file

--- R/change_em_ageerr.R
+++ R/change_em_ageerr.R
@@ -1,37 +1,34 @@
-rm(list=ls());  options(show.error.locations = TRUE)
-library(r4ss)
-dir_in<- "C:/Users/derekc/Desktop/My Files/Research/Florida/Age_Error_Simulation/GT_Simulation_OM_EM/GT_test/om_precision"
-ssfiles <- r4ss::SS_read(dir_in, verbose = FALSE)
-ssfiles$dat$agecomp$Ageerr
-
-names(ssfiles$dat)
-ssfiles$dat$N_ageerror_definitions
-ssfiles$dat$ageerror
-
-defs <- data.frame(
-  "fleet" = 1:2,
-  "definition" = c(1, 1)
-)
 # Example input for ageerr_def
-# Use one value per fleet, not all fleets need to have a value
-list(1 = 2, 2 = 2, 3 = 1)
-# Check to make sure that all values in list are in ageing error definitions
-# use the following function to look up values to see which values
-# need to be changed, then change the value to the given integer from the list
-?match
-# return the dat list so it can be used by the em
-# In simdf use "ca." to send arguments to change_em_ageerr
-change_em_ageerr <- function(dat_list, ageerr_def) {
-  if (!age_err_def %in% seq(dat_list$N_ageerror_definitions)) {
-    stop("Supplied ageing error definition is not in your dat file.")
+defs <- data.frame(
+  "fleet" = c(1:3,5),
+  "definition" = c(1, 3, 2, 3)
+)
+
+change_em_ageerr <- function(dat_list, defs) {
+  #check if Ageerr is being changed
+  change_err<- ifelse(is.null(defs), FALSE, TRUE)
+  if (change_err){
+  # the following code looks up values to see which fleets need to be changed,
+  # then changes the Ageerr value to the given integer from the list
+      for(i in 1:length(defs$fleet)){
+        #check if deginition and fleet are in dat file
+        if (!defs$definition[i] %in% seq(dat_list$N_ageerror_definitions)) {
+          stop("Supplied ageing error definition is not in your dat file.")
+        }
+        if (!defs$fleet[i] %in% unique(dat_list$agecomp$FltSvy)) {
+          stop("Supplied fleet is not in your dat file. Ageing error 
+          definition cannot be changed.")
+        }
+        dat_list$agecomp$Ageerr[dat_list$agecomp$FltSvy 
+        %in% defs$fleet[i]] <- defs$definition[i]
+      }
+
   }
-  dat_list$agecomp$Ageerr<-rep(age_err_def,length(dat_list$agecomp$Ageerr))
+  # return the dat list so it can be used by the em
   return(dat_list)
 }
-test<-change_age_error(ssfiles$dat,age_err_def=2)
-test$agecomp$Ageerr
 
-
+# In simdf use "ca." to send arguments to change_em_ageerr
 # Next after creating function change ss3sim_base to use the function
 # Change scenario stuff to use ca.
 # write a test in test to see if everything works
